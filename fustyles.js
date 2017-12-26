46acae5772518df1962884f69e0c66075e1b104f
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-26 22:43:13 +0800

    Update fustyles.js

--- fustyles.js
+++ fustyles.js
@@ -3,7 +3,7 @@
   'use strict';
 
   function createDiv(input_url_) {
-    var div = document.createElement('div',{ id: 'fustyles'}),
+    var div = document.createElement('div',{ id: 'fustyles'});
     div.innerHTML = '<iframe src="' + input_url_ + '" width="600" height="400">';
     document.body.appendChild(div);
   }
