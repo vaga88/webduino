bbc62ad0e6fc7daa5042c44a1b9cb10da5087d4a
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-26 22:50:39 +0800

    Update javascript.js

--- blockly/javascript.js
+++ blockly/javascript.js
@@ -1,5 +1,5 @@
 Blockly.JavaScript['iframe_show'] = function (block) {
   var value_string_ = Blockly.JavaScript.valueToCode(block, 'string_', Blockly.JavaScript.ORDER_ATOMIC);
   var code = 'createDiv('+value_string_+');';
-  return [code, Blockly.JavaScript.ORDER_FUNCTION_CALL];
+  return code;
 };
