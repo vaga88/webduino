e7585c5a7ac82d93b298e70a03d897b2d99b205e
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-27 01:11:07 +0800

    Update blocks.js

--- blockly/blocks.js
+++ blockly/blocks.js
@@ -4,9 +4,9 @@ Blockly.Blocks['iframe_show'] = {
       .setCheck('String')
       .appendField(Blockly.Msg.TEXT_SHOW)
       .appendField(Blockly.Msg.WIDTH_SHOW)
-      .appendField(new Blockly.FieldNumber(400), "width_")
+      .appendField(new Blockly.FieldNumber(400,0,1000), "width_")
       .appendField(Blockly.Msg.HEIGHT_SHOW)
-      .appendField(new Blockly.FieldNumber(300), "height_")
+      .appendField(new Blockly.FieldNumber(300,0,1000), "height_")
       .appendField(Blockly.Msg.URL_SHOW);
   this.setPreviousStatement(true);
   this.setNextStatement(true);
