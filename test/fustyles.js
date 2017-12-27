0aaf85e7e7ff1b6c3df1227ceee02eeab1cb264a
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-27 22:02:28 +0800

    Update fustyles.js

--- test/fustyles.js
+++ test/fustyles.js
@@ -4,15 +4,20 @@
 
   function createDiv(input_url_,input_WIDTH_,input_HEIGHT_,input_RIGHT_,input_TOP_) {
     
-    var src = '<iframe src="' + input_url_ + '" style="width:' + input_WIDTH_ + 'px;height=' + input_HEIGHT_ + 'px;right=' + input_RIGHT_ + 'px;top=' + input_TOP_ + 'px">';
+    var src = '<iframe src="' + input_url_ + '" style="width:' + input_WIDTH_ + 'px;height=' + input_HEIGHT_ + 'px">';
+    
     if (document.getElementById('fustyles'))
     {
+        document.getElementById('fustyles').right = input_RIGHT_;
+        document.getElementById('fustyles').top = input_TOP_;
         document.getElementById('fustyles').innerHTML = src;
     }
     else
     {
       var div = document.createElement('div');
       div.id = 'fustyles';
+      div.right = input_RIGHT_;
+      div.top = input_TOP_;
       div.innerHTML = src;
       document.body.appendChild(div);
     }
