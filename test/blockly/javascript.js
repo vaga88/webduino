d3ff6cd2419b0b45cef5269d8a40adbb8c5b892f
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-27 21:31:42 +0800

    Update javascript.js

--- test/blockly/javascript.js
+++ test/blockly/javascript.js
@@ -2,6 +2,8 @@ Blockly.JavaScript['iframe_show'] = function (block) {
   var value_url_ = Blockly.JavaScript.valueToCode(block, 'url_', Blockly.JavaScript.ORDER_ATOMIC);
   var value_width_ = Blockly.JavaScript.valueToCode(block, 'width_', Blockly.JavaScript.ORDER_ATOMIC);
   var value_height_ = Blockly.JavaScript.valueToCode(block, 'height_', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = 'createDiv(' + value_url_ + ','+ value_width_ + ',' + value_height_ +');';
+  var value_x_ = Blockly.JavaScript.valueToCode(block, 'x_', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_y_ = Blockly.JavaScript.valueToCode(block, 'y_', Blockly.JavaScript.ORDER_ATOMIC);  
+  var code = 'createDiv(' + value_url_ + ','+ value_width_ + ',' + value_height_ + ',' + value_x_ + ',' + value_y_ + ');';
   return code;
 };
