d641d61a3269bc38a644f025d2c9312cb59e7141
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-27 22:51:37 +0800

    Add files via upload

---
+++ iframe_20171228/blockly/javascript.js
@@ -0,0 +1,9 @@
+Blockly.JavaScript['iframe_show'] = function (block) {
+  var value_url_ = Blockly.JavaScript.valueToCode(block, 'url_', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_width_ = Blockly.JavaScript.valueToCode(block, 'width_', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_height_ = Blockly.JavaScript.valueToCode(block, 'height_', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_left_ = Blockly.JavaScript.valueToCode(block, 'left_', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_top_ = Blockly.JavaScript.valueToCode(block, 'top_', Blockly.JavaScript.ORDER_ATOMIC);  
+  var code = 'createDiv(' + value_url_ + ','+ value_width_ + ',' + value_height_ + ',' + value_left_ + ',' + value_top_ + ');';
+  return code;
+};
---
+++ iframe_20171228/blockly/msg/blocks/zh-hans.js
@@ -0,0 +1,6 @@
+Blockly.Msg.TEXT_SHOW = "显示网页";
+Blockly.Msg.URL_SHOW = "网址";
+Blockly.Msg.WIDTH_SHOW = "宽度";
+Blockly.Msg.HEIGHT_SHOW = "高度";
+Blockly.Msg.LEFT_SHOW = "LEFT";
+Blockly.Msg.TOP_SHOW = "TOP";
