40443c6187d13f12bc073c5ac1ac3847e6ce01f3
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-27 21:49:08 +0800

    Update zh-hant.js

--- test/blockly/msg/blocks/zh-hant.js
+++ test/blockly/msg/blocks/zh-hant.js
@@ -1,6 +1,6 @@
 Blockly.Msg.TEXT_SHOW = "顯示網頁";
+Blockly.Msg.URL_SHOW = "網址";
 Blockly.Msg.WIDTH_SHOW = "寬度";
 Blockly.Msg.HEIGHT_SHOW = "高度";
-Blockly.Msg.URL_SHOW = "網址";
 Blockly.Msg.RIGHT_SHOW = "x";
 Blockly.Msg.TOP_SHOW = "y";
