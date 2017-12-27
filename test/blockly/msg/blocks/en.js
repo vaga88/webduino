7da3e11bd84dba8f83192e6d2beb66d261e9c179
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-27 21:49:25 +0800

    Update en.js

--- test/blockly/msg/blocks/en.js
+++ test/blockly/msg/blocks/en.js
@@ -1,6 +1,6 @@
 Blockly.Msg.TEXT_SHOW = "Web Page";
+Blockly.Msg.URL_SHOW = "url";
 Blockly.Msg.WIDTH_SHOW = "width";
 Blockly.Msg.HEIGHT_SHOW = "height";
-Blockly.Msg.URL_SHOW = "url";
 Blockly.Msg.RIGHT_SHOW = "x";
 Blockly.Msg.TOP_SHOW = "y";
