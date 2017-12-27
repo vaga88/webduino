8b8807c20af38723fde0937ba8fb6296a4299ca9
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-27 08:52:58 +0800

    Add files via upload

---
+++ test/blockly/blocks.js
@@ -0,0 +1,22 @@
+Blockly.Blocks['iframe_show'] = {
+  init: function() {
+  this.appendValueInput("url_")
+      .setCheck("String")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.TEXT_SHOW)
+      .appendField(Blockly.Msg.URL_SHOW)
+  this.appendValueInput("width_")
+      .setCheck("Number")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.WIDTH_SHOW)
+  this.appendValueInput("height_")
+      .setCheck("Number")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.HEIGHT_SHOW);
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setTooltip('');
+  this.setColour(65);
+  this.setHelpUrl(mainUrl + 'basic/blockly/blockly-information.html' + utmUrl);
+  }
+};
