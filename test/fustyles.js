583c9acf26bccfd6164c8bb0b78e4c820f44cc8c
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-27 21:41:35 +0800

    Update fustyles.js

--- test/fustyles.js
+++ test/fustyles.js
@@ -3,7 +3,7 @@
   'use strict';
 
   function createDiv(input_url_,input_WIDTH_,input_HEIGHT_) {
-    var src = '<iframe src="' + input_url_ + '" style="position: relative;width:' + input_WIDTH_ + 'px;height=' + input_HEIGHT_ + 'px;right=' + input_right_ + ';top=' + input_top_ + '">';
+    var src = '<iframe src="' + input_url_ + '" style="position: relative;width:' + input_WIDTH_ + 'px;height=' + input_HEIGHT_ + 'px;right=' + input_right_ + 'px;top=' + input_top_ + 'px">';
     if (document.getElementById('fustyles'))
     {
         document.getElementById('fustyles').innerHTML = src;
