ddfa1c0fd5ba5b7916b73c7da663f30494ef5755
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-29 18:22:33 +0800

    Add files via upload

---
+++ iframe_20171229/blockly/msg/blocks/zh-hans.js
@@ -0,0 +1,7 @@
+Blockly.Msg.TEXT_SHOW = "显示网页";
+Blockly.Msg.URL_SHOW = "网址";
+Blockly.Msg.WIDTH_SHOW = "宽度";
+Blockly.Msg.HEIGHT_SHOW = "高度";
+Blockly.Msg.LEFT_SHOW = "向右距离";
+Blockly.Msg.TOP_SHOW = "向下距离";
+Blockly.Msg.CLOSE_SHOW = "关闭 显示网页";
---
+++ iframe_20171229/fustyles.js
@@ -0,0 +1,39 @@
++(function (window, document) {
+
+  'use strict';
+
+  function createDiv(input_url_,input_WIDTH_,input_HEIGHT_,input_LEFT_,input_TOP_) {
+    
+    var src = '<iframe src="' + input_url_ + '" style="width:' + input_WIDTH_ + 'px;height:' + input_HEIGHT_ + 'px">';
+    
+    if (document.getElementById('fustyles'))
+    {
+        if ((input_WIDTH_<=0)||(input_HEIGHT_<=0))
+        {
+          document.getElementById('fustyles').innerHTML = '';
+          document.getElementById('fustyles').style.display = 'none';
+        }
+        else
+        {
+          document.getElementById('fustyles').style.left = input_LEFT_ + 'px';
+          document.getElementById('fustyles').style.top = input_TOP_ + 'px';
+          document.getElementById('fustyles').style.display = 'block';
+          document.getElementById('fustyles').innerHTML = src;
+        }
+    }
+    else
+    {
+      var div = document.createElement('div');
+      div.id = 'fustyles';
+      div.style.position = "absolute";      
+      div.style.left = input_LEFT_ + 'px';
+      div.style.top = input_TOP_ + 'px';
+      div.style.zindex='9999';      
+      div.innerHTML = src;
+      document.body.appendChild(div);
+    }
+  }
+
+  window.createDiv = createDiv;
+
+}(window, window.document));
