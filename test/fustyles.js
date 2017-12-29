d8ec18f06e4c4144b91f6baf8c585b9f5662461c
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-30 00:44:46 +0800

    Update fustyles.js

--- test/fustyles.js
+++ test/fustyles.js
@@ -1,24 +1,21 @@
 +(function (window, document) {
 
   'use strict';
+  
+  google.load("visualization", "1", { packages: ["corechart"] });
+  google.setOnLoadCallback(drawChart);
 
-  function createDiv(input_url_,input_WIDTH_,input_HEIGHT_,input_LEFT_,input_TOP_) {
-    
-    var src = '<iframe src="' + input_url_ + '" style="width:' + input_WIDTH_ + 'px;height:' + input_HEIGHT_ + 'px">';
+  function createChart(input_value_,input_WIDTH_,input_HEIGHT_,input_LEFT_,input_TOP_) {
     
     if (document.getElementById('fustyles'))
     {
         if ((input_WIDTH_>0)&&(input_HEIGHT_>0))
         {
-          document.getElementById('fustyles').style.left = input_LEFT_ + 'px';
-          document.getElementById('fustyles').style.top = input_TOP_ + 'px';
-          document.getElementById('fustyles').style.display = 'block';
-          document.getElementById('fustyles').innerHTML = src;
+
         }
         else
         {
-          document.getElementById('fustyles').style.display = 'none';
-          document.getElementById('fustyles').innerHTML = '';
+
         }
     }
     else
@@ -26,13 +23,67 @@
       if ((input_WIDTH_>0)&&(input_HEIGHT_>0))
       {
         var div = document.createElement('div');
-        div.id = 'fustyles';
+        div.id = 'fustyles_googlechart';
         div.style.position = "absolute";      
         div.style.left = input_LEFT_ + 'px';
         div.style.top = input_TOP_ + 'px';
-        div.style.zindex='9999';      
-        div.innerHTML = src;
+        div.style.zindex='9999';    
         document.body.appendChild(div);
+        
+        
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'time');
+        data.addColumn('number', 'temperature(°C )');
+        data.addColumn('number', 'humidity( % )');
+
+        var getPara, ParaVal, ParaValTH;
+
+        getPara = input_value_.split(",");
+
+        data.addRows(getPara.length);
+
+        for (i = 0; i < getPara.length; i++) {
+          ParaVal = getPara[i].split("=");
+          ParaValTH = ParaVal[1].split("-");
+          data.setValue(i, 0, ParaVal[0].replace(/_/ig,':'));
+          data.setValue(i, 1, ParaValTH[0]);
+          data.setValue(i, 2, ParaValTH[1]);
+
+          if (i==(getPara.length-1)) {
+            var ShowTH ='溫度 = '+ParaValTH[0].toString()+'°C   '+'濕度 = '+ParaValTH[1].toString()+'%';
+         }
+
+
+        //Line Chart
+        //var chart = new google.visualization.LineChart(document.getElementById('myChart'));
+
+        //Stepped Area Chart
+        var chart = new google.visualization.SteppedAreaChart(document.getElementById('fustyles_googlechart'));
+
+        var options = {
+                  title: ShowTH,
+                  titleTextStyle:{
+                    color: 'red', 
+                    fontName: 'Times New Roman', 
+                    fontSize: 28, 
+                    bold: true,  
+                    italic: false 
+                      },
+          hAxis : { 
+                  textStyle : {fontSize: 14}
+            },
+          vAxis : { 
+                  textStyle : {fontSize: 18}
+            },
+                  allowHtml: true,
+                  showRowNumber: true,
+          width:'100%',
+          height:'400',
+          legend: { position: 'bottom' }
+                  };
+
+        chart.draw(data, options);
+
       }
     }
   }
