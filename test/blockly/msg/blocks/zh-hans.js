49426d5e35de6c5080a151688c906a60bed3b486
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-30 01:20:06 +0800

    Update zh-hans.js

--- test/blockly/msg/blocks/zh-hans.js
+++ test/blockly/msg/blocks/zh-hans.js
@@ -1,7 +1,6 @@
-Blockly.Msg.TEXT_SHOW = "显示网页";
-Blockly.Msg.URL_SHOW = "网址";
+Blockly.Msg.TEXT_SHOW = "折线图";
+Blockly.Msg.VALUE_SHOW = "数值";
 Blockly.Msg.WIDTH_SHOW = "宽度";
 Blockly.Msg.HEIGHT_SHOW = "高度";
 Blockly.Msg.LEFT_SHOW = "右移距离";
 Blockly.Msg.TOP_SHOW = "下移距离";
-Blockly.Msg.CLOSE_SHOW = "关闭 显示网页";
