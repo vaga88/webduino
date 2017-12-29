4173f0275d9a6259f7fc660a84f20e36b96a8a21
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-30 01:58:27 +0800

    Update fustyles.js

--- test/fustyles.js
+++ test/fustyles.js
@@ -30,7 +30,29 @@
         document.body.appendChild(div);
 
         google.load("visualization", "1", { packages: ["corechart"] });
-        google.setOnLoadCallback(drawChart); 
+        google.setOnLoadCallback(drawChart);
+        
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'time');
+        data.addColumn('number', 'temperature(°C )');
+        data.addColumn('number', 'humidity( % )');
+
+        var getPara, ParaVal, ParaValTH;
+
+        getPara = input_value_.split(",");
+
+        data.addRows(getPara.length);
+
+        for (i = 0; i < getPara.length; i++) {
+          ParaVal = getPara[i].split("=");
+          ParaValTH = ParaVal[1].split("-");
+          data.setValue(i, 0, ParaVal[0]);
+          data.setValue(i, 1, ParaValTH[0]);
+          data.setValue(i, 2, ParaValTH[1]);
+
+          if (i==(getPara.length-1)) 
+            var ShowTH ='temperature = '+ParaValTH[0].toString()+'°C   '+'humidity = '+ParaValTH[1].toString()+'%';
+         }
         
         
         
