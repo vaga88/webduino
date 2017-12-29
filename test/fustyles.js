3665530fbe09703ab86f4cb6ce545535e85632b5
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-29 17:01:28 +0800

    Update fustyles.js

--- test/fustyles.js
+++ test/fustyles.js
@@ -10,6 +10,10 @@
     {
         document.getElementById('fustyles').style.left = input_LEFT_ + 'px';
         document.getElementById('fustyles').style.top = input_TOP_ + 'px';
+        if ((input_WIDTH_<=0)||(input_HEIGHT_<=0))
+          document.getElementById('fustyles').style.display = 'none';
+        else
+          document.getElementById('fustyles').style.display = 'block';
         document.getElementById('fustyles').innerHTML = src;
     }
     else
