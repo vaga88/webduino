568100763c582c889b0e35f4a89b6cbd5a71007d
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-31 01:12:20 +0800

    Update javascript.js

--- test/blockly/javascript.js
+++ test/blockly/javascript.js
@@ -2,8 +2,6 @@ Blockly.JavaScript['googlechart_linechart'] = function (block) {
   var value_value_ = Blockly.JavaScript.valueToCode(block, 'value_', Blockly.JavaScript.ORDER_ATOMIC);
   var value_width_ = Blockly.JavaScript.valueToCode(block, 'width_', Blockly.JavaScript.ORDER_ATOMIC);
   var value_height_ = Blockly.JavaScript.valueToCode(block, 'height_', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_left_ = Blockly.JavaScript.valueToCode(block, 'left_', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_top_ = Blockly.JavaScript.valueToCode(block, 'top_', Blockly.JavaScript.ORDER_ATOMIC);  
-  var code = 'createLineChart(' + value_value_ + ','+ value_width_ + ',' + value_height_ + ',' + value_left_ + ',' + value_top_ + ');';
+  var code = 'createLineChart(' + value_value_ + ','+ value_width_ + ',' + value_height_ + ');';
   return code;
 };
