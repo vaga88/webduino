2ba9ee276231cdaa01caef8649cf4774ba88bc1a
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-31 00:35:58 +0800

    Update fustyles.js

--- test/fustyles.js
+++ test/fustyles.js
@@ -90,21 +90,20 @@ svg.append('g')
   .call(d3.axisLeft(yScale))
 
 
-  svg.append("text")             
-      .attr("transform",
-            "translate(" + (width/2) + " ," + 
-                           (height + margin.top + 20) + ")")
-      .style("text-anchor", "middle")
-      .text("Date");
+svg.append("text")             
+    .attr("transform",
+          "translate(" + (width/2) + " ," + (height + margin.top + 20) + ")")
+    .style("text-anchor", "middle")
+    .text("Date");
 
 
-  svg.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left)
-      .attr("x",0 - (height / 2))
-      .attr("dy", "1em")
-      .style("text-anchor", "middle")
-      .text("temperature"); 
+svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x",0 - (height / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text("temperature"); 
         
         
         
