3c34c98bc2890e68bac573dea6d4d60403adbeb8
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-30 19:55:38 +0800

    Update fustyles.js

--- test/fustyles.js
+++ test/fustyles.js
@@ -28,69 +28,62 @@
         document.body.appendChild(s);
         
         
-// Set the dimensions of the canvas / graph
-var margin = {top: 30, right: 20, bottom: 30, left: 50},
-    width = 600 - margin.left - margin.right,
-    height = 270 - margin.top - margin.bottom;
+// csv data (parse data)
+var data = d3.csvParse('date,close\n1-May-12,58.13\n30-Apr-12,53.98\n27-Apr-12,67.00\n26-Apr-12,89.70\n25-Apr-12,99.00\n24-Apr-12,130.28\n23-Apr-12,166.70\n20-Apr-12,234.98\n19-Apr-12,345.44\n18-Apr-12,443.34\n17-Apr-12,543.70\n16-Apr-12,580.13\n13-Apr-12,605.23\n12-Apr-12,622.77\n11-Apr-12,626.20\n10-Apr-12,628.44\n9-Apr-12,636.23\n5-Apr-12,633.68\n4-Apr-12,624.31\n3-Apr-12,629.32\n2-Apr-12,618.63\n30-Mar-12,599.55\n29-Mar-12,609.86\n28-Mar-12,617.62\n27-Mar-12,614.48\n26-Mar-12,606.98\n');
 
-// Parse the date / time
-var parseDate = d3.time.format("%d-%b-%y").parse;
+// === 1. Boilerplate setup
+// Set canvas margins
+var margin = {top: 20, right: 50, bottom: 30, left: 50};
+var width = 800 - margin.left - margin.right;
+var height = 500 - margin.top - margin.bottom;
 
-// Set the ranges
-var x = d3.time.scale().range([0, width]);
-var y = d3.scale.linear().range([height, 0]);
+// Create svg object
+var svg = d3.select('body').append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-// Define the axes
-var xAxis = d3.svg.axis().scale(x)
-    .orient("bottom").ticks(5);
+// === 2. Set scales and draw line
+// Set x (timeseries) and y (linear) scales
+var xScale = d3.scaleTime().range([0, width]);
+var yScale = d3.scaleLinear().range([height, 0]);
 
-var yAxis = d3.svg.axis().scale(y)
-    .orient("left").ticks(5);
+// draw line callback function using d3.line helper passing in x and y coordinates
+var line = d3.line()
+  .x(d => xScale(d.date))
+  .y(d => yScale(d.close))
 
-// Define the line
-var valueline = d3.svg.line()
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.close); });
-    
-// Adds the svg canvas
-var svg = d3.select("body")
-    .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-        .attr("transform", 
-              "translate(" + margin.left + "," + margin.top + ")");
+// === 3. Append data and start drawing
+// create parseTime helper to turn string into time format (11-Apr-12) into datetime JS object
+var parseTime = d3.timeParse('%d-%b-%y');
+// Iterate through each data point and parse strings into time and number format
+data.forEach(function(d){
+  d.date = parseTime(d.date);
+  d.close = parseInt(d.close);
+});
 
-// Get the data
-var data = "date,close\n1-May-12,58.13\n30-Apr-12,53.98\n27-Apr-12,67.00";
+// Set the x and y scales to the data ranges x based on min and max date range (d3.extent()) and y based on 0 to max value
+xScale.domain(d3.extent(data, d => d.date));
+yScale.domain([0, d3.max(data, d => d.close)]);
 
-//d3.csv("data.csv", function(error, data) {
-   data.forEach(function(d) {
-        d.date = parseDate(d.date);
-        d.close = +d.close;
-    });
+// Draw the line svg by appending the data to a new svg path giving a class of line and d value based on the d3.line callback
+svg.append('path')
+  .data([data])
+  .attr('class', 'line')
+  .attr('d', line)
 
-    // Scale the range of the data
-    x.domain(d3.extent(data, function(d) { return d.date; }));
-    y.domain([0, d3.max(data, function(d) { return d.close; })]);
+// Add the axis 
+var xAxis = d3.axisBottom(xScale)
+  .tickFormat(d3.timeFormat('%d-%b'))
+  .ticks(5);
 
-    // Add the valueline path.
-    svg.append("path")
-        .attr("class", "line")
-        .attr("d", valueline(data));
+svg.append('g')
+  .attr('transform', `translate(0, ${height})`)
+  .call(xAxis);
 
-    // Add the X Axis
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
-
-    // Add the Y Axis
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis);
-
-//});
+svg.append('g')
+  .call(d3.axisLeft(yScale))
 
         
           
