d6b3a2b94d341789c1effdc34c9e927fe8b77a7a
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-30 09:20:44 +0800

    Update fustyles.js

--- test/fustyles.js
+++ test/fustyles.js
@@ -28,11 +28,6 @@
         div.style.top = input_TOP_ + 'px';
         div.style.zindex='9999';    
         document.body.appendChild(div);
-        
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.src = 'https://www.gstatic.com/charts/loader.js';    
-        document.getElementsByTagName('head')[0].appendChild(script);
 
         google.load("visualization", "1", { packages: ["corechart"] });
         google.setOnLoadCallback(drawChart);
