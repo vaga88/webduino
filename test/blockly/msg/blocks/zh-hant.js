deff1f97f24349e01528fda1499f49fa84b28977
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-31 01:19:50 +0800

    Update zh-hant.js

--- test/blockly/msg/blocks/zh-hant.js
+++ test/blockly/msg/blocks/zh-hant.js
@@ -2,5 +2,5 @@ Blockly.Msg.TEXT_SHOW = "折線圖";
 Blockly.Msg.VALUE_SHOW = "數值";
 Blockly.Msg.WIDTH_SHOW = "寬度";
 Blockly.Msg.HEIGHT_SHOW = "高度";
-Blockly.Msg.LEFT_SHOW = "右移距離";
-Blockly.Msg.TOP_SHOW = "下移距離";
+Blockly.Msg.TITLE_X_SHOW = "X軸名稱";
+Blockly.Msg.TITLE_Y_SHOW = "Y軸名稱";
