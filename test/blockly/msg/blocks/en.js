79e9db48dbc58dd8098b1389342a6fed2d2f39a2
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-31 01:18:03 +0800

    Update en.js

--- test/blockly/msg/blocks/en.js
+++ test/blockly/msg/blocks/en.js
@@ -1,5 +1,6 @@
 Blockly.Msg.TEXT_SHOW = "Line Chart";
 Blockly.Msg.VALUE_SHOW = "value";
 Blockly.Msg.WIDTH_SHOW = "width";
-Blockly.Msg.LEFT_SHOW = "LEFT";
-Blockly.Msg.TOP_SHOW = "TOP";
+Blockly.Msg.HEIGHT_SHOW = "height";
+Blockly.Msg.TITLE_X_SHOW = "title_x";
+Blockly.Msg.TITLE_Y_SHOW = "title_y";
