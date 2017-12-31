f3410f25b5f593c243aa751e90e0f9d89ebe3a66
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-01 07:37:22 +0800

    Update fustyles.js

--- iframe-20171231/fustyles.js
+++ iframe-20171231/fustyles.js
@@ -76,7 +76,7 @@
       
     svg.append("g")
         .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(xScale).tickFormat(d3.timeFormat("%H:%M:%S")))
+        .call(d3.axisBottom(xScale).ticks(data.length).tickFormat(d3.timeFormat("%H:%M:%S")))
         .selectAll("text")	
           .style("text-anchor", "end")
           .attr("dx", "-.8em")
