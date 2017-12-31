b1c71ee4657069133be9f0d10d61a15622bfe948
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-31 10:20:58 +0800

    Update fustyles.js

--- test/fustyles.js
+++ test/fustyles.js
@@ -44,8 +44,8 @@
       d.humidity = d.humidity;
     });
     
-    var xScale = d3.scaleTime().range([0, width-(margin.top+margin.bottom)]);
-    var yScale = d3.scaleLinear().range([height-(margin.top+margin.bottom), 0]);
+    var xScale = d3.scaleTime().range([0, width]);
+    var yScale = d3.scaleLinear().range([height, 0]);
     
     xScale.domain(d3.extent(data, d => d.time));
     yScale.domain([0, 100]);    
