003ee445da67c63ebc177e6d2be4c5d3b9769aec
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-31 16:02:20 +0800

    Update fustyles.js

--- test/fustyles.js
+++ test/fustyles.js
@@ -39,8 +39,9 @@
     var string = "time,temperature,humidity\n"+input_value_.replace(/&/ig,"\n");
     var data = d3.csvParse(string);
     
+    var parseTime = d3.timeParse("%Y%m%d");
     data.forEach(function(d){
-      d.time = d.time;
+      d.time = parseTime(d.time);
       d.temperature = d.temperature;
       d.humidity = d.humidity;
     });
