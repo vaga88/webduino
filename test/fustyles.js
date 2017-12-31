794f04fc7f133c26e97f4cc697615154b617e3c2
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-31 10:54:19 +0800

    Update fustyles.js

--- test/fustyles.js
+++ test/fustyles.js
@@ -98,13 +98,6 @@
       .attr("r", 4)
       .attr("cx", function(d) { return xScale(d.time); })
       .attr("cy", function(d) { return yScale(d.temperature); })
-    
-    svg.selectAll(".point")
-      .data(data)
-      .enter().append("circle")
-      .attr("class", "point")
-      .attr("r", 4)
-      .attr("cx", function(d) { return xScale(d.time); })
       .attr("cy", function(d) { return yScale(d.humidity); });     
 
   }
