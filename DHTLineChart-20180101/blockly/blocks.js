e34dd22d9c485ffac034083320c9b4259272f629
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-01 12:34:04 +0800

    Add files via upload

---
+++ DHTLineChart-20180101/blockly/blocks.js
@@ -0,0 +1,30 @@
+Blockly.Blocks['DHT_linechart'] = {
+  init: function() {
+  this.appendValueInput("value_")
+      .setCheck("String")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.TEXT_SHOW)
+      .appendField(Blockly.Msg.VALUE_SHOW)
+  this.appendValueInput("width_")
+      .setCheck("Number")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.WIDTH_SHOW)
+  this.appendValueInput("height_")
+      .setCheck("Number")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.HEIGHT_SHOW);
+  this.appendValueInput("title_x_")
+    .setCheck("String")
+    .setAlign(Blockly.ALIGN_RIGHT)
+    .appendField(Blockly.Msg.TITLE_X_SHOW);
+  this.appendValueInput("title_y_")
+    .setCheck("String")
+    .setAlign(Blockly.ALIGN_RIGHT)
+    .appendField(Blockly.Msg.TITLE_Y_SHOW);
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setTooltip('');
+  this.setColour(65);
+  this.setHelpUrl(mainUrl + 'basic/blockly/blockly-information.html' + utmUrl);
+  }
+};
---
+++ DHTLineChart-20180101/blockly/msg/blocks/zh-hant.js
@@ -0,0 +1,6 @@
+Blockly.Msg.TEXT_SHOW = "溫濕度折線圖";
+Blockly.Msg.VALUE_SHOW = "數據";
+Blockly.Msg.WIDTH_SHOW = "寬度";
+Blockly.Msg.HEIGHT_SHOW = "高度";
+Blockly.Msg.TITLE_X_SHOW = "X軸名稱";
+Blockly.Msg.TITLE_Y_SHOW = "Y軸名稱";
