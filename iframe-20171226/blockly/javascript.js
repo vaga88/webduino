25cf73b634d4f3db4224041d0dab69ef141868e3
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-02 23:25:55 +0800

    Delete javascript.js

--- iframe-20171226/blockly/javascript.js
+++
@@ -1,5 +0,0 @@
-Blockly.JavaScript['iframe_show'] = function (block) {
-  var value_string_ = Blockly.JavaScript.valueToCode(block, 'string_', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = 'createDiv('+value_string_+');';
-  return code;
-};
