2a7aca64f19f7c53d48d0c488968a093347d155e
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-03 01:11:14 +0800

    Update javascript.js

--- test/blockly/javascript.js
+++ test/blockly/javascript.js
@@ -4,7 +4,8 @@ Blockly.JavaScript['DHT_chart'] = function (block) {
   var value_width_ = Blockly.JavaScript.valueToCode(block, 'width_', Blockly.JavaScript.ORDER_ATOMIC);
   var value_height_ = Blockly.JavaScript.valueToCode(block, 'height_', Blockly.JavaScript.ORDER_ATOMIC);
   var value_title_x_ = Blockly.JavaScript.valueToCode(block, 'title_x_', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_title_y_ = Blockly.JavaScript.valueToCode(block, 'title_y_', Blockly.JavaScript.ORDER_ATOMIC);  
-  var code = 'createDHTchart(' + dropdown_type_ + ',' + value_value_ + ','+ value_width_ + ',' + value_height_ + ',' + value_title_x_ + ',' + value_title_y_ + ');';
+  var value_title_y_ = Blockly.JavaScript.valueToCode(block, 'title_y_', Blockly.JavaScript.ORDER_ATOMIC);
+  alert(dropdown_type_);
+  var code = 'createDHTchart("' + dropdown_type_ + '","' + value_value_ + '",'+ value_width_ + ',' + value_height_ + ',' + value_title_x_ + ',' + value_title_y_ + ');';
   return code;
 };
