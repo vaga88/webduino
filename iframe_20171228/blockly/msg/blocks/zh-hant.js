190c8d4b6c9c76cb5285a5c95b26d2f924745f1d
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-02 23:29:48 +0800

    Delete zh-hant.js

--- iframe_20171228/blockly/msg/blocks/zh-hant.js
+++
@@ -1,6 +0,0 @@
-Blockly.Msg.TEXT_SHOW = "顯示網頁";
-Blockly.Msg.URL_SHOW = "網址";
-Blockly.Msg.WIDTH_SHOW = "寬度";
-Blockly.Msg.HEIGHT_SHOW = "高度";
-Blockly.Msg.LEFT_SHOW = "LEFT";
-Blockly.Msg.TOP_SHOW = "TOP";
