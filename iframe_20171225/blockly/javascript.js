67f6eb3be07ea7b98559bd1f3a7b38db8588a1ea
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-02 23:24:08 +0800

    Delete javascript.js

--- iframe_20171225/blockly/javascript.js
+++
@@ -1,5 +0,0 @@
-Blockly.JavaScript['iframe_show'] = function (block) {
-  var value_string_ = Blockly.JavaScript.valueToCode(block, 'string_', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = ' document.getElementById("demo-area-01-show").innerHTML = "<iframe src='+ value_string_ +' width=600 height=400>";';
-  return code;
-};
