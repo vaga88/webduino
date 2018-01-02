eac12da5e5efc2859859e52462b8184b3ef4be37
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-03 01:32:04 +0800

    Add files via upload

---
+++ DHTLineChart-20180102/blockly/msg/blocks/en.js
@@ -0,0 +1,6 @@
+Blockly.Msg.TEXT_SHOW = " DHT";
+Blockly.Msg.VALUE_SHOW = "value";
+Blockly.Msg.WIDTH_SHOW = "width";
+Blockly.Msg.HEIGHT_SHOW = "height";
+Blockly.Msg.TITLE_X_SHOW = "title_x";
+Blockly.Msg.TITLE_Y_SHOW = "title_y";
