177baa8ab8a539501d65dbcd9d9ffc08a1c02f87
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-03 21:59:27 +0800

    Update zh-hans.js

--- DHTchart-20180103/blockly/msg/blocks/zh-hans.js
+++ DHTchart-20180103/blockly/msg/blocks/zh-hans.js
@@ -6,4 +6,4 @@ Blockly.Msg.TITLE_X_SHOW = "X轴名称";
 Blockly.Msg.TITLE_Y_SHOW = "Y轴名称";
 Blockly.Msg.BLOCK2_SHOW = "取得";
 Blockly.Msg.DATASET_SHOW = "数据";
-Blockly.Msg.COUNT_SHOW = "笔数";
+Blockly.Msg.COUNT_SHOW = "最新笔数";
