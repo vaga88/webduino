efba86e75c63c551be05ddf44c60540da8b67f7e
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-03 18:02:21 +0800

    Update en.js

--- test/blockly/msg/blocks/en.js
+++ test/blockly/msg/blocks/en.js
@@ -1,6 +1,9 @@
-Blockly.Msg.TEXT_SHOW = " DHT";
+Blockly.Msg.BLOCK1_SHOW = " DHT";
 Blockly.Msg.VALUE_SHOW = "value";
 Blockly.Msg.WIDTH_SHOW = "width";
 Blockly.Msg.HEIGHT_SHOW = "height";
 Blockly.Msg.TITLE_X_SHOW = "title_x";
 Blockly.Msg.TITLE_Y_SHOW = "title_y";
+Blockly.Msg.BLOCK2_SHOW = " Check Dataset";
+Blockly.Msg.DATASET_SHOW = "DATASET";
+Blockly.Msg.COUNT_SHOW = "COUNT";
