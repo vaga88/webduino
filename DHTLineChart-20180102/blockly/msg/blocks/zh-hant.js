a09d991075776f2debbe7e4e6840a8f27e839572
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-06 15:53:52 +0800

    Delete zh-hant.js

--- DHTLineChart-20180102/blockly/msg/blocks/zh-hant.js
+++
@@ -1,6 +0,0 @@
-Blockly.Msg.TEXT_SHOW = "溫濕度";
-Blockly.Msg.VALUE_SHOW = "數據";
-Blockly.Msg.WIDTH_SHOW = "寬度";
-Blockly.Msg.HEIGHT_SHOW = "高度";
-Blockly.Msg.TITLE_X_SHOW = "X軸名稱";
-Blockly.Msg.TITLE_Y_SHOW = "Y軸名稱";
