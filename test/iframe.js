e0517a4fa8c457e13f247c71ea9b3ab46875c67c
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-07 12:39:02 +0800

    Update iframe.js

--- test/iframe.js
+++ test/iframe.js
@@ -10,9 +10,7 @@
     if (("."+substr[substr.length-1]+",").indexOf(".aac,.au,.aif,.aiff,.aiffc,.alac,.ape,.asf,.avi,.cda,.dat,.divx,.flac,.m2ts,.m4a,.mid,.mov,.mp2,.mp3,.mp4,.mpc,.mpg,.mpeg,.mv2,.ogg,.pdf,.ra,.ram,.raw,.rcp,.rm,.rmvb,.snd,.tak,.tta,.vob,.voc,.wma,.wav,.wmf,.wmv,.wv,.xmi,")!=-1)
         var src = '<embed src="' + input_url_ + '" style="width:' + input_WIDTH_ + 'px;height:' + input_HEIGHT_ + 'px" autostart="true"></embed>'; 
     else if (("."+substr[substr.length-1]+",").indexOf(".swf,.flv,")!=-1)
-    {
         var src = '<embed src="' + input_url_ + '" style="width:' + input_WIDTH_ + 'px;height:' + input_HEIGHT_ + 'px" type="application/x-shockwave-flash"></embed>';
-    }
     else
         var src = '<iframe src="' + input_url_ + '" style="width:' + input_WIDTH_ + 'px;height:' + input_HEIGHT_ + 'px"></iframe>';
     
