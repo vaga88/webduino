7d4a155318af2604a607a23a3669de74525e11b7
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-07 22:32:13 +0800

    Update en.js

--- test_Matrix LED/blockly/msg/blocks/en.js
+++ test_Matrix LED/blockly/msg/blocks/en.js
@@ -1,8 +1,1 @@
-Blockly.Msg.OPEN_SHOW = "Web Page";
-Blockly.Msg.CLOSE_SHOW = "Close Web Page";
-Blockly.Msg.OPENID_SHOW = "ID";
-Blockly.Msg.CLOSEID_SHOW = "ID";
-Blockly.Msg.URL_SHOW = "URL";
-Blockly.Msg.WIDTH_SHOW = "WIDTH";
-Blockly.Msg.LEFT_SHOW = "LEFT";
-Blockly.Msg.TOP_SHOW = "TOP";
+MATRIXLED_SHOW = "MATRIX LED";
