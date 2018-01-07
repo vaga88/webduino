eae95f43e18f90b18b0b421703268a740f1c6160
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-07 14:43:31 +0800

    Update en.js

--- test/blockly/msg/blocks/en.js
+++ test/blockly/msg/blocks/en.js
@@ -1,6 +1,7 @@
 Blockly.Msg.TEXT_SHOW = "Web Page";
-Blockly.Msg.URL_SHOW = "url";
-Blockly.Msg.WIDTH_SHOW = "width";
+Blockly.Msg.URL_SHOW = "ID";
+Blockly.Msg.URL_SHOW = "URL";
+Blockly.Msg.WIDTH_SHOW = "WIDTH";
 Blockly.Msg.LEFT_SHOW = "LEFT";
 Blockly.Msg.TOP_SHOW = "TOP";
 Blockly.Msg.CLOSE_SHOW = "Close Web Page";
