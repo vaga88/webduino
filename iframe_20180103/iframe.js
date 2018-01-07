d00601ac7cb6aca732ec7129bdb3f051386eaa4f
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-07 12:29:46 +0800

    Update iframe.js

--- iframe_20180103/iframe.js
+++ iframe_20180103/iframe.js
@@ -6,7 +6,7 @@
     
     var substr = input_url_.toLowerCase().split('.');
     
-    if (substr[substr.length-1].indexOf("aac,au,aif,aiff,aiffc,alac,ape,asf,avi,cda,dat,divx,flac,m2ts,m4a,mid,mov,mp2,mp3,mp4,mpc,mpg,mpeg,mv2,ogg,pdf,ra,ram,raw,rcp,rm,rmvb,snd,tak,tta,vob,voc,wma,wav,wmf,wmv,wv,xmi")!=-1)
+    if (substr[substr.length-1].indexOf("aac,au,aif,aiff,aiffc,alac,ape,asf,avi,cda,dat,divx,flac,m2ts,m4a,mid,mov,mp2,mp3,mp4,mpc,mpg,mpeg,mv2,ogg,pdf,ra,ram,raw,rcp,rm,rmvb,snd,tak,tta,vob,voc,wma,wav,webm,wmf,wmv,wv,xmi")!=-1)
         var src = '<embed src="' + input_url_ + '" style="width:' + input_WIDTH_ + 'px;height:' + input_HEIGHT_ + 'px" autostart="true"></embed>'; 
     else if ((substr[substr.length-1]=='swf')||(substr[substr.length-1]=='flv'))
         var src = '<embed src="' + input_url_ + '" style="width:' + input_WIDTH_ + 'px;height:' + input_HEIGHT_ + 'px" type="application/x-shockwave-flash"></embed>';
