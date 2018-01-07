cdee8950c9957aafd63d892f9d4ae1d0d883ed8b
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-07 23:12:02 +0800

    Update matrixled.js

--- test_Matrix LED/matrixled.js
+++ test_Matrix LED/matrixled.js
@@ -12,7 +12,7 @@
         L[i]=" bgcolor='white'";
     }
     
-    var ledtable ="<table style='width:250px;height:250px;border:0;'>"
+    var ledtable ="<table style='width:250px;height:250px;border:white;'>"
                   + "<tr><td"+L[0]+"></td><td"+L[1]+"></td><td"+L[2]+"></td><td"+L[3]+"></td><td"+L[4]+"></td></tr>"
                   + "<tr><td"+L[5]+"></td><td"+L[6]+"></td><td"+L[7]+"></td><td"+L[8]+"></td><td"+L[9]+"></td></tr>"
                   + "<tr><td"+L[10]+"></td><td"+L[11]+"></td><td"+L[12]+"></td><td"+L[13]+"></td><td"+L[14]+"></td></tr>"
