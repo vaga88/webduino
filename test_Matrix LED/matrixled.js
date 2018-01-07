1a85cbe7acd768e0d74f4c7623131057792a8ea8
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-07 22:20:43 +0800

    Update matrixled.js

--- test_Matrix LED/matrixled.js
+++ test_Matrix LED/matrixled.js
@@ -4,7 +4,22 @@
 
   function createMatrixLed(input_value_) {
     
-    var ledtable ="";
+    var L[25];
+    for (var i=0;i<25;i++)
+    {
+      if (input_value_.substr(i,1)=="1")
+        L[i]=" bgcolor='red'";
+      else
+        L[i]=" bgcolor='white'";
+    }
+    
+    var ledtable ="<table width=400 height=400 border=0>"
+                  + "<tr><td"+L[0]+"></td><td"+L[1]+"></td><td"+L[2]+"></td><td"+L[3]+"></td><td"+L[4]+"></td></tr>"
+                  + "<tr><td"+L[5]+"></td><td"+L[6]+"></td><td"+L[7]+"></td><td"+L[8]+"></td><td"+L[9]+"></td></tr>"
+                  + "<tr><td"+L[10]+"></td><td"+L[11]+"></td><td"+L[12]+"></td><td"+L[13]+"></td><td"+L[14]+"></td></tr>"
+                  + "<tr><td"+L[15]+"></td><td"+L[16]+"></td><td"+L[17]+"></td><td"+L[18]+"></td><td"+L[19]+"></td></tr>"
+                  + "<tr><td"+L[20]+"></td><td"+L[21]+"></td><td"+L[22]+"></td><td"+L[23]+"></td><td"+L[24]+"></td></tr>"
+                  + "</table>";
       
     if (document.getElementById("fustyles_matrixled"))
     {
