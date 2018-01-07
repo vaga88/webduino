8f4c2564a1c4884e5afe24529bb8a0c84e39c793
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-07 13:20:29 +0800

    Update iframe.js

--- iframe_20180107/iframe.js
+++ iframe_20180107/iframe.js
@@ -7,9 +7,9 @@
     var substr = input_url_.toLowerCase().split('.');
   
     if (".aac,.au,.aif,.aiff,.aiffc,.alac,.ape,.asf,.avi,.cda,.dat,.divx,.flac,.m2ts,.m4a,.mid,.mov,.mp2,.mp3,.mp4,.mpc,.mpg,.mpeg,.mv2,.ogg,.pdf,.ra,.ram,.raw,.rcp,.rm,.rmvb,.snd,.tak,.tta,.vob,.voc,.wma,.wav,.webm,.wmf,.wmv,.wv,.xmi,".indexOf("."+substr[substr.length-1]+",")!=-1)
-        var src = '<embed src="' + input_url_ + '" style="width:' + input_WIDTH_ + 'px;height:' + input_HEIGHT_ + 'px" palette="white|white" autostart="true"></embed>'; 
+        var src = '<embed src="' + input_url_ + '" style="width:' + input_WIDTH_ + 'px;height:' + input_HEIGHT_ + 'px" autostart="true"></embed>'; 
     else if (".swf,.flv,".indexOf("."+substr[substr.length-1]+",")!=-1)
-        var src = '<embed src="' + input_url_ + '" style="width:' + input_WIDTH_ + 'px;height:' + input_HEIGHT_ + 'px" palette="white|white" type="application/x-shockwave-flash"></embed>';
+        var src = '<embed src="' + input_url_ + '" style="width:' + input_WIDTH_ + 'px;height:' + input_HEIGHT_ + 'px" type="application/x-shockwave-flash"></embed>';
     else
         var src = '<iframe src="' + input_url_ + '" style="width:' + input_WIDTH_ + 'px;height:' + input_HEIGHT_ + 'px"></iframe>';
     
