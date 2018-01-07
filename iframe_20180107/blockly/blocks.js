ffcd313d380bab1de1fb16af8a7d12004efa6e84
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-07 18:55:08 +0800

    Add files via upload

--- iframe_20180107/blockly/blocks.js
+++ iframe_20180107/blockly/blocks.js
@@ -1,9 +1,13 @@
 Blockly.Blocks['iframe_open'] = {
   init: function() {
+  this.appendValueInput("openid_")
+      .setCheck("Number")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.OPEN_SHOW)
+      .appendField(Blockly.Msg.OPENID_SHOW)    
   this.appendValueInput("url_")
       .setCheck("String")
       .setAlign(Blockly.ALIGN_RIGHT)
-      .appendField(Blockly.Msg.TEXT_SHOW)
       .appendField(Blockly.Msg.URL_SHOW)
   this.appendValueInput("width_")
       .setCheck("Number")
@@ -30,8 +34,11 @@ Blockly.Blocks['iframe_open'] = {
 };
 Blockly.Blocks['iframe_close'] = {
   init: function() {
-    this.appendDummyInput()
+    this.appendValueInput("closeid_")
+        .setCheck("Number")
+        .setAlign(Blockly.ALIGN_RIGHT)
         .appendField(Blockly.Msg.CLOSE_SHOW)
+        .appendField(Blockly.Msg.CLOSEID_SHOW)     
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(65);
--- iframe_20180107/blockly/msg/blocks/en.js
+++ iframe_20180107/blockly/msg/blocks/en.js
@@ -1,6 +1,8 @@
-Blockly.Msg.TEXT_SHOW = "Web Page";
-Blockly.Msg.URL_SHOW = "url";
-Blockly.Msg.WIDTH_SHOW = "width";
+Blockly.Msg.OPEN_SHOW = "Web Page";
+Blockly.Msg.CLOSE_SHOW = "Close Web Page";
+Blockly.Msg.OPENID_SHOW = "ID";
+Blockly.Msg.CLOSEID_SHOW = "ID";
+Blockly.Msg.URL_SHOW = "URL";
+Blockly.Msg.WIDTH_SHOW = "WIDTH";
 Blockly.Msg.LEFT_SHOW = "LEFT";
 Blockly.Msg.TOP_SHOW = "TOP";
-Blockly.Msg.CLOSE_SHOW = "Close Web Page";
