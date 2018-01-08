dfbfd953c1f5446dd32efcc43138d5b10aeace70
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-09 00:01:42 +0800

    Update zh-hant.js

--- test/blockly/msg/blocks/zh-hant.js
+++ test/blockly/msg/blocks/zh-hant.js
@@ -2,3 +2,6 @@ Blockly.Msg.MATRIXLED_MATRIX_SHOW = "顯示 指示燈";
 Blockly.Msg.MATRIXLED_CLEAR_SHOW = "清除 屏幕";
 Blockly.Msg.MATRIXLED_ON_SHOW = "點亮";
 Blockly.Msg.MATRIXLED_OFF_SHOW = "不點亮";
+Blockly.Msg.MATRIXLED_COLOR_SHOW = "燈光顏色";
+Blockly.Msg.MATRIXLED_WIDTH_SHOW = "屏幕寬度";
+Blockly.Msg.MATRIXLED_HEIGHT_SHOW = "屏幕高度";
