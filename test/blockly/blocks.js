cd155be34018819c916f5e8b019c0c65d57ae47b
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-08 18:50:01 +0800

    Update blocks.js

--- test/blockly/blocks.js
+++ test/blockly/blocks.js
@@ -4,34 +4,34 @@ Blockly.Blocks['matrix_led'] = {
         .setAlign(Blockly.ALIGN_CENTRE)
         .appendField(Blockly.Msg.MATRIXLED_SHOW);
     this.appendDummyInput()
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L1")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L2")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L3")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L4")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L5");
-    this.appendDummyInput()
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L6")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L7")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L8")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L9")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L10");
-    this.appendDummyInput()
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L01")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L06")
         .appendField(new Blockly.FieldCheckbox("FALSE"), "L11")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L12")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L13")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L14")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L15");
-    this.appendDummyInput()
         .appendField(new Blockly.FieldCheckbox("FALSE"), "L16")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L17")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L18")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L19")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L20");
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L21");
     this.appendDummyInput()
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L21")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L22")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L23")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L24")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L02")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L07")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L12")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L17")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L22");
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L03")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L08")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L13")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L18")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L23");
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L04")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L09")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L14")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L19")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L24");
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L05")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L10")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L15")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L20")
         .appendField(new Blockly.FieldCheckbox("FALSE"), "L25");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
