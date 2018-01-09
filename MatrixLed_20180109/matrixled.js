1cf4487014c8f94d335915406db92236c1b0307c
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-09 18:47:58 +0800

    Add files via upload

---
+++ MatrixLed_20180109/matrixled.js
@@ -0,0 +1,100 @@
++(function (window, document) {
+
+  'use strict';
+  
+  var L = new Array("","","","","","","","","","","","","","","","","","","","","","","","","");
+  var MatrixLedcolor = "#FF0000";
+  var MatrixLedwidth = 250;
+  var MatrixLedheight = 250;
+
+  function MatrixLed_color(input_color_) {
+    MatrixLedcolor = input_color_;
+  }
+  
+  function MatrixLed_width(input_width_) {
+    MatrixLedwidth = input_width_;
+  }
+  
+  function MatrixLed_height(input_height_) {
+    MatrixLedheight = input_height_;
+  }  
+  
+  function MatrixLed_clear() {
+    for (var i=0;i<25;i++)
+    {
+      L[i]=" bgcolor='white'";
+    }
+    MatrixLed_show();
+  }
+  
+  function MatrixLed_on(input_x_,input_y_) {
+    for (var i=0;i<25;i++)
+    {
+      if (i==(input_x_*5+input_y_))
+        L[i]=" bgcolor='"+MatrixLedcolor+"'";
+      else
+      {
+        if (L[i]=="")
+          L[i]=" bgcolor='white'";
+      }
+    }
+    MatrixLed_show();
+  }
+  
+  function MatrixLed_off(input_x_,input_y_) {
+    for (var i=0;i<25;i++)
+    {
+      if (i==(input_x_*5+input_y_))
+        L[i]=" bgcolor='white'";
+      else
+      {
+        if (L[i]=="")
+          L[i]=" bgcolor='white'";
+      }
+    }
+    MatrixLed_show();
+  }
+  
+  function MatrixLed_matrix(input_value_) {
+    for (var i=0;i<25;i++)
+    {
+      if (input_value_.substr(i,1)=="1")
+        L[i]=" bgcolor='"+MatrixLedcolor+"'";
+      else
+        L[i]=" bgcolor='white'";
+    }
+    MatrixLed_show();
+  }
+  
+  function MatrixLed_show() {
+    var ledtable ="<table style='width:"+MatrixLedwidth+"px;height:"+MatrixLedheight+"px;border:white;'>"
+                  + "<tr><td"+L[0]+"></td><td"+L[5]+"></td><td"+L[10]+"></td><td"+L[15]+"></td><td"+L[20]+"></td></tr>"
+                  + "<tr><td"+L[1]+"></td><td"+L[6]+"></td><td"+L[11]+"></td><td"+L[16]+"></td><td"+L[21]+"></td></tr>"
+                  + "<tr><td"+L[2]+"></td><td"+L[7]+"></td><td"+L[12]+"></td><td"+L[17]+"></td><td"+L[22]+"></td></tr>"
+                  + "<tr><td"+L[3]+"></td><td"+L[8]+"></td><td"+L[13]+"></td><td"+L[18]+"></td><td"+L[23]+"></td></tr>"
+                  + "<tr><td"+L[4]+"></td><td"+L[9]+"></td><td"+L[14]+"></td><td"+L[19]+"></td><td"+L[24]+"></td></tr>"
+                  + "</table>";
+      
+    if (document.getElementById("fustyles_matrixled"))
+    {
+          document.getElementById("fustyles_matrixled").innerHTML = ledtable;
+    }
+    else
+    {
+        var div = document.createElement('div');
+        div.id = "fustyles_matrixled";   
+        div.innerHTML = ledtable;
+        document.body.appendChild(div);
+    }    
+  }
+
+  window.MatrixLed_clear = MatrixLed_clear;
+  window.MatrixLed_matrix = MatrixLed_matrix;
+  window.MatrixLed_show = MatrixLed_show;
+  window.MatrixLed_on = MatrixLed_on;
+  window.MatrixLed_off = MatrixLed_off;
+  window.MatrixLed_color = MatrixLed_color;
+  window.MatrixLed_width = MatrixLed_width;
+  window.MatrixLed_height = MatrixLed_height;
+
+}(window, window.document));
