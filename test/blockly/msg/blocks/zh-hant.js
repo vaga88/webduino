19cdf4a4e387f07919c29274ae98bdf6684b78b5
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-09 15:55:13 +0800

    Update zh-hant.js

--- test/blockly/msg/blocks/zh-hant.js
+++ test/blockly/msg/blocks/zh-hant.js
@@ -1,7 +1,8 @@
 Blockly.Msg.MATRIXLED_MATRIX_SHOW = "顯示 指示燈";
 Blockly.Msg.MATRIXLED_CLEAR_SHOW = "清除 指示燈";
-Blockly.Msg.MATRIXLED_ON_SHOW = "點燈";
-Blockly.Msg.MATRIXLED_OFF_SHOW = "熄燈";
+Blockly.Msg.MATRIXLED_ON_SHOW = "亮燈";
+Blockly.Msg.MATRIXLED_OFF_SHOW = "暗燈";
 Blockly.Msg.MATRIXLED_COLOR_SHOW = "燈光顏色";
-Blockly.Msg.MATRIXLED_WIDTH_SHOW = "指示燈寬度";
-Blockly.Msg.MATRIXLED_HEIGHT_SHOW = "指示燈高度";
+Blockly.Msg.MATRIXLED_WIDTH_SHOW = "指示燈 寬度";
+Blockly.Msg.MATRIXLED_HEIGHT_SHOW = "指示燈 高度";
+Blockly.Msg.MATRIXLED_SAMPLE_SHOW = "顯示 範本";
