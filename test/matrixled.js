28a8f1a0c75f736c8d1c39b53b3d9f03c87cf73e
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-09 23:06:58 +0800

    Update matrixled.js

--- test/matrixled.js
+++ test/matrixled.js
@@ -21,7 +21,7 @@
   }  
   
   function MatrixLed_marquee(input_marquee_) {
-    MatrixLedmarquee = input_marquee_;
+    MatrixLedmarquee = input_marquee_.toUpperCase();
     alert(MatrixLedmarquee);
   }  
   
@@ -92,6 +92,39 @@
         div.innerHTML = ledtable;
         document.body.appendChild(div);
     }    
+  }
+  
+  function MatrixLed_conversion(input_str_) {
+    const A = "";
+    const B = "";
+    const C = "";
+    const D = "";
+    const E = "";
+    const F = "";
+    const G = "";
+    const H = "";
+    const I = "";
+    const J = "";
+    const K = "";
+    const L = "";
+    const M = "";
+    const N = "";
+    const O = "";
+    const P = "";
+    const Q = "";
+    const R = "";
+    const S = "";
+    const T = "";
+    const U = "";
+    const V = "";
+    const W = "";
+    const X = "";
+    const Y = "";   
+    const Z = "";
+    const , = "";
+    const . = "";
+    const ? = "";
+    const ! = "";    
   }
 
   window.MatrixLed_clear = MatrixLed_clear;
