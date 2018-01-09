3133692aa793c1ed658a4f3883851ce5b7358612
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-10 01:34:26 +0800

    Add files via upload

---
+++ MatrixLed_20180110/blockly/msg/blocks/zh-hans.js
@@ -0,0 +1,10 @@
+Blockly.Msg.MATRIXLED_MATRIX_SHOW = "显示 指示灯";
+Blockly.Msg.MATRIXLED_CLEAR_SHOW = "清除 指示灯";
+Blockly.Msg.MATRIXLED_ON_SHOW = "亮灯";
+Blockly.Msg.MATRIXLED_OFF_SHOW = "暗灯";
+Blockly.Msg.MATRIXLED_COLOR_SHOW = "灯光颜色";
+Blockly.Msg.MATRIXLED_WIDTH_SHOW = "指示灯 宽度";
+Blockly.Msg.MATRIXLED_HEIGHT_SHOW = "指示灯 高度";
+Blockly.Msg.MATRIXLED_SAMPLE_SHOW = "显示 范本";
+Blockly.Msg.MATRIXLED_MARQUEE_SHOW = "显示 跑马灯(英数)";
+Blockly.Msg.MATRIXLED_CHAR_SHOW = "显示 字元(英数)";
