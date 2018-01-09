8ebf18245607b0367ed642f65ba564dd0eb3472c
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-09 12:03:12 +0800

    Update zh-hant.js

--- MatrixLed_20180108/blockly/msg/blocks/zh-hant.js
+++ MatrixLed_20180108/blockly/msg/blocks/zh-hant.js
@@ -1,7 +1,7 @@
 Blockly.Msg.MATRIXLED_MATRIX_SHOW = "顯示 指示燈";
-Blockly.Msg.MATRIXLED_CLEAR_SHOW = "清除 屏幕";
-Blockly.Msg.MATRIXLED_ON_SHOW = "點亮";
-Blockly.Msg.MATRIXLED_OFF_SHOW = "不點亮";
+Blockly.Msg.MATRIXLED_CLEAR_SHOW = "清除 螢幕";
+Blockly.Msg.MATRIXLED_ON_SHOW = "點燈";
+Blockly.Msg.MATRIXLED_OFF_SHOW = "熄燈";
 Blockly.Msg.MATRIXLED_COLOR_SHOW = "燈光顏色";
-Blockly.Msg.MATRIXLED_WIDTH_SHOW = "屏幕寬度";
-Blockly.Msg.MATRIXLED_HEIGHT_SHOW = "屏幕高度";
+Blockly.Msg.MATRIXLED_WIDTH_SHOW = "螢幕寬度";
+Blockly.Msg.MATRIXLED_HEIGHT_SHOW = "螢幕高度";
