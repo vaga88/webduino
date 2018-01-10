2e01d16967b45e92c7a912c2689f4fbf7e7f7eab
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-11 00:41:53 +0800

    Update blocks.js

--- MatrixLed_20180110/blockly/blocks.js
+++ MatrixLed_20180110/blockly/blocks.js
@@ -15,7 +15,9 @@ Blockly.Blocks['matrix_led_marquee'] = {
   init: function() {
     this.appendValueInput("value_marquee_")
         .setCheck("String")   
-        .appendField(Blockly.Msg.MATRIXLED_MARQUEE_SHOW);    
+        .appendField(Blockly.Msg.MATRIXLED_MARQUEE_SHOW)
+        .appendField(new Blockly.FieldNumber(500), "value_marquee_time_")
+        .appendField(Blockly.Msg.MATRIXLED_MARQUEE_TIME_SHOW);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(300);    
