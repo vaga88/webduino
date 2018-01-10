c34b8efea0dae5eb9c5a4f3ca00a70ff7026e49c
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-10 19:17:17 +0800

    Update matrixled.js

--- test/matrixled.js
+++ test/matrixled.js
@@ -91,6 +91,7 @@
       for (var i=0;i<input_marquee_.length;i++)
         MatrixLedmarquee = MatrixLedmarquee + MatrixLed_conversion(input_marquee_.substr(i,1).toUpperCase()) + "0000000000";
     }
+    window.clearInterval(timeid);
     timeid = window.setInterval("MatrixLed_marquee_play()",MatrixLedmarqueetime);
   }  
   
