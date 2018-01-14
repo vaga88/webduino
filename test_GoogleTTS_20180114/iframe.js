75fcf6313814ae46a52f872974d6294b2aea362b
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-14 11:29:01 +0800

    Update iframe.js

--- test_GoogleTTS_20180114/iframe.js
+++ test_GoogleTTS_20180114/iframe.js
@@ -6,7 +6,7 @@
     
     var myWindow = window.open("https://translate.google.com.tw/?hl=zh-TW#zh-CN/en/"+input_word_,"googletts");
    
-    myWindow.document.getElementById("googletts").contentWindow.document.getElementById('gt-res-listen').class="trans-listen-button goog-toolbar-button goog-toolbar-button-checked";
+    myWindow.document.getElementById('gt-res-listen').class="trans-listen-button goog-toolbar-button goog-toolbar-button-checked";
     //document.getElementById("googletts").contentWindow.document.getElementById('gt-res-listen').aria-pressed="true";
   }
 
