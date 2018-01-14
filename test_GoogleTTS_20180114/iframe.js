fa0c2510cb846005cd1ed7701b3a36fac48987a0
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-14 11:38:21 +0800

    Update iframe.js

--- test_GoogleTTS_20180114/iframe.js
+++ test_GoogleTTS_20180114/iframe.js
@@ -7,7 +7,7 @@
     var myWindow = window.open("https://translate.google.com.tw/?hl=zh-TW#zh-CN/en/"+input_word_,"_blank");
    
     myWindow.document.getElementById('gt-res-listen').class="trans-listen-button goog-toolbar-button goog-toolbar-button-checked";
-    myWindow.document.getElementById('gt-res-listen')..aria-pressed="true";
+    myWindow.document.getElementById('gt-res-listen').aria-pressed="true";
   }
 
   window.createIframe = createIframe;
