bd5af42366ef492d178149a022cb052e2a9c469e
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-14 12:12:43 +0800

    Update iframe.js

--- test_GoogleTTS_20180114/iframe.js
+++ test_GoogleTTS_20180114/iframe.js
@@ -4,7 +4,7 @@
 
   function createIframe(input_id_,input_word_,input_WIDTH_,input_HEIGHT_,input_LEFT_,input_TOP_) {
     
-    var getcode = new ActiveXObject("Msxml2.XMLHTTP");
+    var getcode = new XMLHttpRequest();
     getcode.onReadyStateChange = getgoogletranslate;
     getcode.open("GET", "https://translate.google.com.tw/?hl=zh-TW#zh-CN/en/"+input_word_ , true);
     getcode.send(null);				
