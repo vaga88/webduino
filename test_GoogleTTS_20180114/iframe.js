646549514f5c0794a5f6bb999890b2f680d90924
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-14 13:44:50 +0800

    Update iframe.js

--- test_GoogleTTS_20180114/iframe.js
+++ test_GoogleTTS_20180114/iframe.js
@@ -3,7 +3,7 @@
   'use strict';
  
 
-  function GoogleTTS(input_url_,input_sheet_,input_word_,input_x_,input_y_) {
+  function GoogleTTS(input_url_,input_sheet_,input_word_,input_x_,input_y_,input_x_get_,input_y_get_) {
 
     var myData= {};
     myData.sheetUrl = input_url_;
@@ -13,8 +13,8 @@
     writeSheetData(myData);
     
     readSheetData({
-      row : 1,
-      col : 2,
+      row : input_x_get_,
+      col : input_y_get_,
       sheetUrl : myData.sheetUrl,
       sheetName : myData.sheetName
     },function(googleSheetReadData){
