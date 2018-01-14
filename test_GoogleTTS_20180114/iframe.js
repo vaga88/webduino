85a5dbfae35ac06fcefe09270eccb7c1a6a5c8d5
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-14 12:38:16 +0800

    Update iframe.js

--- test_GoogleTTS_20180114/iframe.js
+++ test_GoogleTTS_20180114/iframe.js
@@ -3,7 +3,6 @@
   'use strict';
 
   function createIframe(input_id_,input_word_,input_WIDTH_,input_HEIGHT_,input_LEFT_,input_TOP_) {
-
     var request = createCORSRequest("get", "https://translate.google.com.tw/?hl=zh-TW#zh-CN/en/"+input_word_);
     if (request){
         request.onload = function(){
@@ -11,21 +10,22 @@
         };
         request.send();
     }
+  }
     
-    function createCORSRequest(method, url){
-        var xhr = new XMLHttpRequest();
-        if ("withCredentials" in xhr){
-            xhr.open(method, url, true);
-        } else if (typeof XDomainRequest != "undefined"){
-            xhr = new XDomainRequest();
-            xhr.open(method, url);
-        } else {
-            xhr = null;
-        }
-        return xhr;
+  function createCORSRequest(method, url){
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr){
+        xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined"){
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+    } else {
+        xhr = null;
     }
+    return xhr;
+  }
     
-    window.createIframe = createIframe;
-    window.createCORSRequest = createCORSRequest;
+  window.createIframe = createIframe;
+  window.createCORSRequest = createCORSRequest;
     
 }(window, window.document));
