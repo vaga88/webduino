182608d827258035b0ca0cd8f18da7e2dcbb4cf9
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-14 11:35:54 +0800

    Update iframe.js

--- test_GoogleTTS_20180114/iframe.js
+++ test_GoogleTTS_20180114/iframe.js
@@ -4,7 +4,7 @@
 
   function createIframe(input_id_,input_word_,input_WIDTH_,input_HEIGHT_,input_LEFT_,input_TOP_) {
     
-    var myWindow = window.open("https://translate.google.com.tw/?hl=zh-TW#zh-CN/en/"+input_word_,"googletts");
+    var myWindow = window.open("https://translate.google.com.tw/?hl=zh-TW#zh-CN/en/"+input_word_,"_blank");
    
     myWindow.document.getElementById('gt-res-listen').class="trans-listen-button goog-toolbar-button goog-toolbar-button-checked";
     myWindow.document.getElementById('gt-res-listen')..aria-pressed="true";
