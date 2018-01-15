7d5ce61c7654f0d4b0ff51f5f928f5ad399afa9e
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-15 13:33:55 +0800

    Update blocks.js

--- test/blockly/blocks.js
+++ test/blockly/blocks.js
@@ -78,8 +78,8 @@ Blockly.Blocks['matrix_led_sample'] = {
     this.appendDummyInput()
         .appendField(Blockly.Msg.MATRIXLED_SAMPLE_SHOW)
         .appendField(new Blockly.FieldDropdown([["♥","♥"], ["♡","♡"], ["↑","↑"], ["↓","↓"], ["←","←"], ["→","→"], ["↖","↖"], ["↙","↙"], ["↗","↗"], ["↘","↘"], ["▲","▲"], ["▼","▼"], ["◄","◄"], ["►","►"], ["O","O"], ["X","X"], ["V","V"]]), "value_sample_");     
-    this.setPreviousStatement(true);
-    this.setNextStatement(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
     this.setTooltip('');
     this.setColour(300);
     this.setHelpUrl("");
@@ -175,6 +175,48 @@ Blockly.Blocks['matrix_led_matrix'] = {
         .appendField(new Blockly.FieldCheckbox("FALSE"), "L25");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
+    this.setColour(300);    
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['matrix_led_matrixcode'] = {
+  init: function() {
+    this.appendDummyInput()
+        .setAlign(Blockly.ALIGN_CENTRE)
+        .appendField(Blockly.Msg.MATRIXLED_MATRIX_SHOW);
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L01")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L06")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L11")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L16")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L21");
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L02")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L07")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L12")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L17")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L22");
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L03")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L08")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L13")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L18")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L23");
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L04")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L09")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L14")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L19")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L24");
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L05")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L10")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L15")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L20")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "L25");
+    this.setOutput(true, null);
     this.setColour(300);    
     this.setTooltip("");
     this.setHelpUrl("");
