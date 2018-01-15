d90da5d25135c1aab62684558a94a364cdc51972
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-16 07:53:59 +0800

    Update blocks.js

--- test/blockly/blocks.js
+++ test/blockly/blocks.js
@@ -276,6 +276,13 @@ Blockly.Blocks['matrix_led_state'] = {
   init: function() {
     this.appendDummyInput()
         .appendField(Blockly.Msg.MATRIXLED_STATE_SHOW);
+    this.appendValueInput("value_x_state_")
+        .setCheck("Number")
+        .appendField("x");
+    this.appendValueInput("value_y_state_")
+        .setCheck("Number")
+        .appendField("y");
+    this.setInputsInline(true);
     this.setOutput(true, null);  
     this.setColour(300);
     this.setTooltip("");
