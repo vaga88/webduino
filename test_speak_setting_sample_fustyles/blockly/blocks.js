9a4867d4ba093d228d34979c605da45a3ca3c493
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-16 22:31:23 +0800

    Update blocks.js

--- test_speak_setting_sample_fustyles/blockly/blocks.js
+++ test_speak_setting_sample_fustyles/blockly/blocks.js
@@ -1,293 +1,47 @@
-Blockly.Blocks['matrix_led_char'] = {
-  init: function() {
-    this.appendValueInput("value_char_")
-        .setCheck(null)   
-        .appendField(Blockly.Msg.MATRIXLED_CHAR_SHOW);    
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(300);    
-    this.setTooltip("");
-    this.setHelpUrl("");    
-  }
-}; 
-
-Blockly.Blocks['matrix_led_marquee'] = {
-  init: function() {
-    this.appendValueInput("value_marquee_")
-        .setCheck("String")   
-        .appendField(Blockly.Msg.MATRIXLED_MARQUEE_SHOW); 
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(300);    
-    this.setTooltip("");
-    this.setHelpUrl("");    
-  }
-};  
-
-Blockly.Blocks['matrix_led_marquee_stop'] = {
-  init: function() {
+Blockly.Blocks['speak_setting_sample_fustyles'] = {
+  init: function () {
     this.appendDummyInput()
-        .appendField(Blockly.Msg.MATRIXLED_MARQUEE_STOP_SHOW);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(300);    
-    this.setTooltip("");
-    this.setHelpUrl("");    
-  }
-}; 
-
-Blockly.Blocks['matrix_led_marquee_resume'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField(Blockly.Msg.MATRIXLED_MARQUEE_RESUME_SHOW);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(300);    
-    this.setTooltip("");
-    this.setHelpUrl("");    
-  }
-}; 
-
-Blockly.Blocks['matrix_led_marquee_reverse'] = {
-  init: function() {
-    this.appendDummyInput() 
-        .appendField(Blockly.Msg.MATRIXLED_MARQUEE_REVERSE_SHOW);    
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(300);    
-    this.setTooltip("");
-    this.setHelpUrl("");    
-  }
-};
-
-Blockly.Blocks['matrix_led_marquee_time'] = {
-  init: function() {
-    this.appendValueInput("value_marquee_time_")
-        .setCheck("Number")  
-        .appendField(Blockly.Msg.MATRIXLED_MARQUEE_TIME_SHOW);  
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(300);    
-    this.setTooltip("");
-    this.setHelpUrl("");    
-  }
-};  
-
-Blockly.Blocks['matrix_led_sample'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField(Blockly.Msg.MATRIXLED_SAMPLE_SHOW)
-        .appendField(new Blockly.FieldDropdown([["♥","♥"], ["♡","♡"], ["↑","↑"], ["↓","↓"], ["←","←"], ["→","→"], ["↖","↖"], ["↙","↙"], ["↗","↗"], ["↘","↘"], ["▲","▲"], ["▼","▼"], ["◄","◄"], ["►","►"], ["O","O"], ["X","X"], ["V","V"]]), "value_sample_");     
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setTooltip('');
-    this.setColour(300);
-    this.setHelpUrl("");
-  }
-};
-
-Blockly.Blocks['matrix_led_code'] = {
-  init: function() {
-    this.appendValueInput("value_code_")
-        .setCheck("String")   
-        .appendField(Blockly.Msg.MATRIXLED_CODE_SHOW)
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(300);    
-    this.setTooltip("");
-    this.setHelpUrl("");    
-  }
-}; 
-
-Blockly.Blocks['matrix_led_color'] = {
-  init: function() {
-    this.appendValueInput("value_color_")
-        .setCheck(null)  
-        .appendField(Blockly.Msg.MATRIXLED_COLOR_SHOW);    
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(300);    
-    this.setTooltip("");
-    this.setHelpUrl("");    
-  }
-};  
-
-Blockly.Blocks['matrix_led_width'] = {
-  init: function() {
-    this.appendValueInput("value_width_")
-        .setCheck("Number")  
-        .appendField(Blockly.Msg.MATRIXLED_WIDTH_SHOW);    
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(300);    
-    this.setTooltip("");
-    this.setHelpUrl("");    
-  }
-};  
-
-Blockly.Blocks['matrix_led_height'] = {
-  init: function() {
-    this.appendValueInput("value_height_")
-        .setCheck("Number")  
-        .appendField(Blockly.Msg.MATRIXLED_HEIGHT_SHOW);    
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(300);    
-    this.setTooltip("");
-    this.setHelpUrl("");    
-  }
-};   
-
-Blockly.Blocks['matrix_led_matrix'] = {
-  init: function() {
-    this.appendDummyInput()
-        .setAlign(Blockly.ALIGN_CENTRE)
-        .appendField(Blockly.Msg.MATRIXLED_MATRIX_SHOW);
-    this.appendDummyInput()
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L01")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L06")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L11")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L16")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L21");
-    this.appendDummyInput()
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L02")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L07")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L12")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L17")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L22");
-    this.appendDummyInput()
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L03")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L08")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L13")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L18")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L23");
-    this.appendDummyInput()
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L04")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L09")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L14")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L19")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L24");
-    this.appendDummyInput()
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L05")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L10")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L15")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L20")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L25");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(300);    
-    this.setTooltip("");
-    this.setHelpUrl("");
-  }
-};
-
-Blockly.Blocks['matrix_led_matrixcode'] = {
-  init: function() {
-    this.appendDummyInput()
-        .setAlign(Blockly.ALIGN_CENTRE)
-        .appendField(Blockly.Msg.MATRIXLED_MATRIXCODE_SHOW);
-    this.appendDummyInput()
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L01")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L06")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L11")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L16")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L21");
-    this.appendDummyInput()
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L02")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L07")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L12")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L17")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L22");
-    this.appendDummyInput()
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L03")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L08")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L13")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L18")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L23");
-    this.appendDummyInput()
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L04")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L09")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L14")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L19")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L24");
-    this.appendDummyInput()
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L05")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L10")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L15")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L20")
-        .appendField(new Blockly.FieldCheckbox("FALSE"), "L25");
+      .appendField(Blockly.Msg.WEBDUINO_SPEAK_LANG)
+      .appendField(new Blockly.FieldDropdown([
+        [Blockly.Msg.WEBDUINO_SPEAK_TW, "zh-TW"],
+        [Blockly.Msg.WEBDUINO_SPEAK_US, "en-US"],
+        [Blockly.Msg.WEBDUINO_SPEAK_JP, "ja-JP"],
+        [Blockly.Msg.WEBDUINO_SPEAK_JP, "ko-KR"]
+      ]), "lang_")
+      .appendField(Blockly.Msg.WEBDUINO_SPEAK_VOLUME)
+      .appendField(new Blockly.FieldDropdown([
+        ["1", "1"],
+        ["0.9", "0.9"],
+        ["0.8", "0.8"],
+        ["0.7", "0.7"],
+        ["0.6", "0.6"],
+        ["0.5", "0.5"],
+        ["0.4", "0.4"],
+        ["0.3", "0.3"],
+        ["0.2", "0.3"],
+        ["0.1", "0.1"],
+        ["0", "0"]
+      ]), "volume_")
+      .appendField(Blockly.Msg.WEBDUINO_SPEAK_PITCH)
+      .appendField(new Blockly.FieldDropdown([
+        [Blockly.Msg.WEBDUINO_SPEAK_P20, "2"],
+        [Blockly.Msg.WEBDUINO_SPEAK_P15, "1.5"],
+        [Blockly.Msg.WEBDUINO_SPEAK_P10, "1"],
+        [Blockly.Msg.WEBDUINO_SPEAK_P05, "0.5"],
+        [Blockly.Msg.WEBDUINO_SPEAK_P01, "0.1"]
+      ]), "pitch_")
+      .appendField(Blockly.Msg.WEBDUINO_SPEAK_RATE)
+      .appendField(new Blockly.FieldDropdown([
+        [Blockly.Msg.WEBDUINO_SPEAK_R20, "2"],
+        [Blockly.Msg.WEBDUINO_SPEAK_R15, "1.5"],
+        [Blockly.Msg.WEBDUINO_SPEAK_R10, "1"],
+        [Blockly.Msg.WEBDUINO_SPEAK_R07, "0.7"],
+        [Blockly.Msg.WEBDUINO_SPEAK_R05, "0.5"]
+      ]), "rate_");
+    this.setInputsInline(true);
     this.setOutput(true, null);
-    this.setColour(300);    
-    this.setTooltip("");
-    this.setHelpUrl("");
+    this.setTooltip('');
+    this.setColour(270);
+    this.setHelpUrl(mainUrl + 'useful/component/buzzer-clock.html' + utmUrl); 
   }
 };
-
-Blockly.Blocks['matrix_led_clear'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField(Blockly.Msg.MATRIXLED_CLEAR_SHOW);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(300);    
-    this.setTooltip("");
-    this.setHelpUrl("");    
-  }
-};    
-
-Blockly.Blocks['matrix_led_on'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField(Blockly.Msg.MATRIXLED_ON_SHOW);
-    this.appendValueInput("value_x_on_")
-        .setCheck("Number")
-        .appendField("x");
-    this.appendValueInput("value_y_on_")
-        .setCheck("Number")
-        .appendField("y");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(300);
-    this.setTooltip("");
-    this.setHelpUrl("");
-  }
-};
-
-Blockly.Blocks['matrix_led_off'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField(Blockly.Msg.MATRIXLED_OFF_SHOW);
-    this.appendValueInput("value_x_off_")
-        .setCheck("Number")
-        .appendField("x");
-    this.appendValueInput("value_y_off_")
-        .setCheck("Number")
-        .appendField("y");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(300);
-    this.setTooltip("");
-    this.setHelpUrl("");
-  }
-};  
-  
-Blockly.Blocks['matrix_led_state'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField(Blockly.Msg.MATRIXLED_STATE_SHOW);
-    this.appendValueInput("value_x_state_")
-        .setCheck("Number")
-        .appendField("x");
-    this.appendValueInput("value_y_state_")
-        .setCheck("Number")
-        .appendField("y");
-    this.setInputsInline(true);
-    this.setOutput(true, null);  
-    this.setColour(300);
-    this.setTooltip("");
-    this.setHelpUrl("");
-  }  
-};
-
