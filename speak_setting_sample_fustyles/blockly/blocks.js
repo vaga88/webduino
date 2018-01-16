5be0f873eb2b1e0c860652f0b582075551a6c9c8
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-16 23:46:47 +0800

    Update blocks.js

--- speak_setting_sample_fustyles/blockly/blocks.js
+++ speak_setting_sample_fustyles/blockly/blocks.js
@@ -7,7 +7,7 @@ Blockly.Blocks['speak_setting_sample_fustyles'] = {
         [Blockly.Msg.WEBDUINO_SPEAK_US, "en-US"],
         [Blockly.Msg.WEBDUINO_SPEAK_JP, "ja-JP"],
         [Blockly.Msg.WEBDUINO_SPEAK_KR, "ko-KR"],
-        [Blockly.Msg.WEBDUINO_SPEAK_TH, "th-TH"],
+        [Blockly.Msg.WEBDUINO_SPEAK_TH, "th"],
         [Blockly.Msg.WEBDUINO_SPEAK_VI, "vi-VN"],      
         [Blockly.Msg.WEBDUINO_SPEAK_ES, "es-ES"],
         [Blockly.Msg.WEBDUINO_SPEAK_FR, "fr-FR"],
