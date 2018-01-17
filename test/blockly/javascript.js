29f6d0b02019cc6e72806bc75c107f64fc6b307e
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-18 01:17:44 +0800

    Add files via upload

--- test/blockly/javascript.js
+++ test/blockly/javascript.js
@@ -8,10 +8,10 @@ Blockly.JavaScript['speak_setting_sample_fustyles'] = function (block) {
 };
 
 Blockly.JavaScript['speak_setting_sample1_fustyles'] = function (block) {
-  var value_lang_ =Blockly.JavaScript.valueToCode(block, 'value_lang_', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_lang_ = Blockly.JavaScript.valueToCode(block, 'value_lang_', Blockly.JavaScript.ORDER_ATOMIC);
   var dropdown_volume_ = block.getFieldValue('volume_');
   var dropdown_pitch_ = block.getFieldValue('pitch_');
   var dropdown_rate_ = block.getFieldValue('rate_');
-  var code = value_lang_ + ',' + dropdown_volume_ + ',' + dropdown_pitch_ + ',' + dropdown_rate_;
+  var code = '"' + value_lang_.replace(/\'/g,"") + '",' + dropdown_volume_ + ',' + dropdown_pitch_ + ',' + dropdown_rate_;
   return [code, Blockly.JavaScript.ORDER_ATOMIC];
 };
