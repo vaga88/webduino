c61ffc37bceb0ffc95d46221df175c0044182a7e
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-20 10:41:55 +0800

    Update blocks.js

--- test_PM2.5_gov/blockly/blocks.js
+++ test_PM2.5_gov/blockly/blocks.js
@@ -2,12 +2,21 @@ Blockly.Blocks['pm_gov'] = {
   init: function() {
     this.appendDummyInput()
         .appendField(Blockly.Msg.PM_SHOW);
-    this.appendValueInput("value_county_")
-        .setCheck("Number")
-        .appendField("x");
-    this.appendValueInput("value_url_")
-        .setCheck("Number")
-        .appendField("y");
+        .appendField(new Blockly.FieldDropdown([
+          ["1", "1"],
+          ["0.9", "0.9"],
+          ["0.8", "0.8"],
+          ["0.7", "0.7"],
+          ["0.6", "0.6"],
+          ["0.5", "0.5"],
+          ["0.4", "0.4"],
+          ["0.3", "0.3"],
+          ["0.2", "0.3"],
+          ["0.1", "0.1"],
+          ["0", "0"]
+        ]), "volume_")
+        .appendValueInput("value_url_")
+        .setCheck("String");
     this.setInputsInline(true);
     this.setOutput(true, null);  
     this.setColour(300);
