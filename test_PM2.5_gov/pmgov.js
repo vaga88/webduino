fd4d6111ee0cf2cca69c237416123daa9f49b860
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-20 12:29:01 +0800

    Update pmgov.js

--- test_PM2.5_gov/pmgov.js
+++ test_PM2.5_gov/pmgov.js
@@ -50,10 +50,9 @@
                           x[i].getElementsByTagName("WindDirec")[0].childNodes[0].nodeValue +
                           ",WindSpeed," +
                           x[i].getElementsByTagName("WindSpeed")[0].childNodes[0].nodeValue; 
-                alert(Data);
-                return data;
+                callback.apply(data);
               }
-              if (i==(x.length-1)) alert("No Exist!");
+              if (i==(x.length-1)) callback.apply("No Exist!");
             }
       }
     };
