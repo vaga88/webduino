c748735293c8bb55d9e8a8c4e7ac8290a54981a5
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-20 13:33:35 +0800

    Update javascript.js

--- test_PM2.5_gov/blockly/javascript.js
+++ test_PM2.5_gov/blockly/javascript.js
@@ -2,5 +2,12 @@ Blockly.JavaScript['pm_gov'] = function(block) {
   var value_url_ = Blockly.JavaScript.valueToCode(block, 'value_url_', Blockly.JavaScript.ORDER_ATOMIC);  
   var value_site_ = block.getFieldValue('value_site_');
   var code = 'PM_gov_get('+value_url_+',"'+value_site_+'")';
+  for (var name in window)
+  {
+    document.write(window[name]);
+  }
+  return("");
+}
+
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
