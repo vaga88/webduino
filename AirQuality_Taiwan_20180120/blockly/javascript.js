42b497a962014cf61dd24f5f70a57a9618b1a812
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-20 20:09:50 +0800

    Add files via upload

---
+++ AirQuality_Taiwan_20180120/blockly/javascript.js
@@ -0,0 +1,11 @@
+Blockly.JavaScript['pmgov_link'] = function(block) {
+  var value_url_ = Blockly.JavaScript.valueToCode(block, 'value_url_', Blockly.JavaScript.ORDER_ATOMIC);  
+  var code = 'PM_gov_link('+value_url_+');await delay(1);';
+  return code;
+};
+
+Blockly.JavaScript['pmgov_get'] = function(block) {
+  var value_site_ = block.getFieldValue('value_site_');  
+  var code = 'PM_gov_get("'+value_site_+'")';
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
