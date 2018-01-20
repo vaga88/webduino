fc30a7818c5c16cf4161eba0db9ca6e6cf0ffb2e
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-21 01:18:13 +0800

    Update AirQualityTaiwan.js

--- AirQuality_Taiwan_20180120/AirQualityTaiwan.js
+++ AirQuality_Taiwan_20180120/AirQualityTaiwan.js
@@ -2,18 +2,18 @@
 
   'use strict';
   
+  var PM_data;
+  
   function PM_gov_link(input_url_) 
   {
-    if (document.getElementById("fustyles_PM"))
-      document.getElementById("fustyles_PM").innerHTML = "";
-
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() 
     {
       if (this.readyState == 4 && this.status == 200) 
       {
         var x = this.responseXML.getElementsByTagName("Data");
-        var PM_data = "";
+        PM_data = "";
+        
         for (var i = 0; i <x.length; i++) 
         { 
           PM_data = PM_data + "County,";
@@ -33,7 +33,7 @@
             PM_data = PM_data + x[i].getElementsByTagName("PM10")[0].childNodes[0].nodeValue;
           PM_data = PM_data + ",PM10_AVG,";
           if (x[i].getElementsByTagName("PM10_AVG")[0].childNodes.length==1)                      
-            PM_data = PM_data + x[i].getElementsByTagName("PM10_AVG")[0].childNodes[0].nodeValu;                      
+            PM_data = PM_data + x[i].getElementsByTagName("PM10_AVG")[0].childNodes[0].nodeValue;                    
           PM_data = PM_data + ",PM2.5,";
           if (x[i].getElementsByTagName("PM2.5")[0].childNodes.length==1)                      
             PM_data = PM_data + x[i].getElementsByTagName("PM2.5")[0].childNodes[0].nodeValue;
@@ -72,28 +72,6 @@
             PM_data = PM_data + x[i].getElementsByTagName("WindSpeed")[0].childNodes[0].nodeValue; 
           PM_data = PM_data + ";"; 
         }
-        if (document.getElementById("fustyles_PM"))
-        {
-              document.getElementById("fustyles_PM").style.left = '0px';
-              document.getElementById("fustyles_PM").style.top = '0px';
-              document.getElementById("fustyles_PM").style.width = '0px';
-              document.getElementById("fustyles_PM").style.height = '0px';
-              document.getElementById("fustyles_PM").style.display = 'none';
-              document.getElementById("fustyles_PM").innerHTML = PM_data;
-        }
-        else
-        {
-            var div = document.createElement('div');
-            div.id = "fustyles_PM";    
-            div.style.left = '0px';
-            div.style.top = '0px';
-            div.style.width = '0px';
-            div.style.height = '0px';                        
-            div.style.zindex='9999';   
-            div.style.display="none";
-            div.innerHTML = PM_data;
-            document.body.appendChild(div);
-        }
       }
     };
     xmlHttp.open("GET", input_url_, true);
@@ -102,18 +80,12 @@
   
   function PM_gov_get(input_site_) 
   {
-    if (document.getElementById("fustyles_PM"))
-    {
-      var x = (document.getElementById("fustyles_PM").innerHTML).split(";");
-      var s = input_site_.split("-");
+    var x = PM_data.split(";");
+    var s = input_site_.split("-");
 
-      for (var i = 0; i <(x.length-1); i++) 
-      { 
-        if ((x[i].indexOf(s[0])!=-1)&&(x[i].indexOf(s[1])!=-1))
-        {
-          return x[i];
-         }
-      }
+    for (var i = 0; i <(x.length-1); i++) 
+    { 
+      if ((x[i].indexOf(s[0])!=-1)&&(x[i].indexOf(s[1])!=-1)) return x[i];
     }
   } 
   
