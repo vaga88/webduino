e7db654e5103caf964119f437e41934234805579
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-20 10:33:25 +0800

    Update pm2.5gov.js

--- test_PM2.5_gov/pm2.5gov.js
+++ test_PM2.5_gov/pm2.5gov.js
@@ -2,43 +2,36 @@
 
   'use strict';
 
-  function PM2.5_gov_get(input_url_,input_county_) {
+  function PM2.5_gov_get(input_url_,input_county_) 
+  {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() 
     {
-      if (this.readyState == 4 && this.status == 200) {
-        myFunction(this,input_county_);
+      if (this.readyState == 4 && this.status == 200) 
+      {
+            var x = this.responseXML.getElementsByTagName("Data");
+            for (var i = 0; i <x.length; i++) 
+            { 
+              if ((x[i].getElementsByTagName("SiteName")[0].childNodes[0].nodeValue+"-"+x[i].getElementsByTagName("County")[0].childNodes[0].nodeValue)==input_county_)
+                  return "SiteName," +
+                          x[i].getElementsByTagName("SiteName")[0].childNodes[0].nodeValue +
+                          ",County," +
+                        x[i].getElementsByTagName("County")[0].childNodes[0].nodeValue +
+                          ",PublishTime," +
+                        x[i].getElementsByTagName("PublishTime")[0].childNodes[0].nodeValue +
+                          ",PM10," +
+                        x[i].getElementsByTagName("PM10")[0].childNodes[0].nodeValue +
+                          ",PM2.5," +
+                        x[i].getElementsByTagName("PM2.5")[0].childNodes[0].nodeValue +
+                          ",Status," +
+                          x[i].getElementsByTagName("Status")[0].childNodes[0].nodeValue;
+            }
       }
     };
     xhttp.open("GET", input_url_, true);
     xhttp.send();
   }
-
-  function PM2.5_gov_return(xml,county) {
-
-    var xmlDoc = xml.responseXML;
-    var table="<tr><th>SiteName</th><th>County</th><th>PublishTime</th><th>PM10</th><th>PM2.5</th><th>Status</th></tr>";
-    var x = xmlDoc.getElementsByTagName("Data");
-    for (var i = 0; i <x.length; i++) { 
-      table += "<tr><td>" +
-      x[i].getElementsByTagName("SiteName")[0].childNodes[0].nodeValue +
-      "</td><td>" +
-    x[i].getElementsByTagName("County")[0].childNodes[0].nodeValue +
-      "</td><td>" +
-    x[i].getElementsByTagName("PublishTime")[0].childNodes[0].nodeValue +
-      "</td><td>" +
-    x[i].getElementsByTagName("PM10")[0].childNodes[0].nodeValue +
-      "</td><td>" +
-    x[i].getElementsByTagName("PM2.5")[0].childNodes[0].nodeValue +
-      "</td><td>" +
-      x[i].getElementsByTagName("Status")[0].childNodes[0].nodeValue +
-      "</td></tr>";
-    }
-    return  "<table >" +　table + "</table>";
-  }
-
-
+    
   window.PM2.5_gov_get = PM2.5_gov_get;
-  window.PM2.5_gov_return = PM2.5_gov_return;
   
 }(window, window.document));
