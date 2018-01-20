d5135ec09ef77ee349aa2e8710ffba38d6a67c68
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-20 16:56:01 +0800

    Update pmgov.js

--- test_PM2.5_gov/pmgov.js
+++ test_PM2.5_gov/pmgov.js
@@ -17,58 +17,58 @@
         for (var i = 0; i <x.length; i++) 
         { 
           PM_data = PM_data + "SiteName,";
-          if (x[i].getElementsByTagName("SiteName").HasChildNodes==true)
+          if (x[i].getElementsByTagName("SiteName").HasChildNodes())
             PM_data = PM_data + x[i].getElementsByTagName("SiteName")[0].childNodes[0].nodeValue;
           PM_data = PM_data + ",County,";
-          if (x[i].getElementsByTagName("County").HasChildNodes==true)                      
+          if (x[i].getElementsByTagName("County").HasChildNodes())                      
             PM_data = PM_data + x[i].getElementsByTagName("County")[0].childNodes[0].nodeValue;
           PM_data = PM_data + ",PublishTime,";
-          if (x[i].getElementsByTagName("PublishTime").HasChildNodes==true)                      
+          if (x[i].getElementsByTagName("PublishTime").HasChildNodes())                      
             PM_data = PM_data + x[i].getElementsByTagName("PublishTime")[0].childNodes[0].nodeValue;
           PM_data = PM_data + ",PM10,";
-          if (x[i].getElementsByTagName("PM10").HasChildNodes==true)                      
+          if (x[i].getElementsByTagName("PM10").HasChildNodes())                      
             PM_data = PM_data + x[i].getElementsByTagName("PM10")[0].childNodes[0].nodeValue;
           PM_data = PM_data + ",PM10_AVG,";
-          if (x[i].getElementsByTagName("PM10_AVG").HasChildNodes==true)                      
+          if (x[i].getElementsByTagName("PM10_AVG").HasChildNodes())                      
             PM_data = PM_data + x[i].getElementsByTagName("PM10_AVG")[0].childNodes[0].nodeValu;                      
           PM_data = PM_data + ",PM2.5,";
-          if (x[i].getElementsByTagName("PM2.5").HasChildNodes==true)                      
+          if (x[i].getElementsByTagName("PM2.5").HasChildNodes())                      
             PM_data = PM_data + x[i].getElementsByTagName("PM2.5")[0].childNodes[0].nodeValue;
           PM_data = PM_data + ",PM2.5_AVG,";
-          if (x[i].getElementsByTagName("PM2.5_AVG").HasChildNodes==true)                      
+          if (x[i].getElementsByTagName("PM2.5_AVG").HasChildNodes())                      
             PM_data = PM_data + x[i].getElementsByTagName("PM2.5_AVG")[0].childNodes[0].nodeValue;
           PM_data = PM_data + ",Status,";
-          if (x[i].getElementsByTagName("Status").HasChildNodes==true)                      
+          if (x[i].getElementsByTagName("Status").HasChildNodes())                      
             PM_data = PM_data + x[i].getElementsByTagName("Status")[0].childNodes[0].nodeValue;
           PM_data = PM_data + ",CO,";
-          if (x[i].getElementsByTagName("CO").HasChildNodes==true)                      
+          if (x[i].getElementsByTagName("CO").HasChildNodes())                      
             PM_data = PM_data + x[i].getElementsByTagName("CO")[0].childNodes[0].nodeValue;
           PM_data = PM_data + ",CO_8hr,";
-          if (x[i].getElementsByTagName("CO_8hr").HasChildNodes==true)                      
+          if (x[i].getElementsByTagName("CO_8hr").HasChildNodes())                      
             PM_data = PM_data + x[i].getElementsByTagName("CO_8hr")[0].childNodes[0].nodeValue;
           PM_data = PM_data + ",NO,";
-          if (x[i].getElementsByTagName("NO").HasChildNodes==true)                      
+          if (x[i].getElementsByTagName("NO").HasChildNodes())                      
             PM_data = PM_data + x[i].getElementsByTagName("NO")[0].childNodes[0].nodeValue;
           PM_data = PM_data + ",NO2,";
-          if (x[i].getElementsByTagName("NO2").HasChildNodes==true)                      
+          if (x[i].getElementsByTagName("NO2").HasChildNodes())                      
             PM_data = PM_data + x[i].getElementsByTagName("NO2")[0].childNodes[0].nodeValue;  
           PM_data = PM_data + ",NOx,";
-          if (x[i].getElementsByTagName("NOx").HasChildNodes==true)                      
+          if (x[i].getElementsByTagName("NOx").HasChildNodes())                      
             PM_data = PM_data + x[i].getElementsByTagName("NOx")[0].childNodes[0].nodeValue;
           PM_data = PM_data + ",O3,";
-          if (x[i].getElementsByTagName("O3").HasChildNodes==true)                      
+          if (x[i].getElementsByTagName("O3").HasChildNodes())                      
             PM_data = PM_data + x[i].getElementsByTagName("O3")[0].childNodes[0].nodeValue;
           PM_data = PM_data + ",O3_8hr,";
-          if (x[i].getElementsByTagName("O3_8hr").HasChildNodes==true)                      
+          if (x[i].getElementsByTagName("O3_8hr").HasChildNodes())                      
             PM_data = PM_data + x[i].getElementsByTagName("O3_8hr")[0].childNodes[0].nodeValue;
           PM_data = PM_data + ",SO2,";
-          if (x[i].getElementsByTagName("SO2").HasChildNodes==true)                      
+          if (x[i].getElementsByTagName("SO2").HasChildNodes())                      
             PM_data = PM_data + x[i].getElementsByTagName("SO2")[0].childNodes[0].nodeValue;
           PM_data = PM_data + ",WindDirec,";
-          if (x[i].getElementsByTagName("WindDirec").HasChildNodes==true)
+          if (x[i].getElementsByTagName("WindDirec").HasChildNodes())
             PM_data = PM_data + x[i].getElementsByTagName("WindDirec")[0].childNodes[0].nodeValue;
           PM_data = PM_data + ",WindSpeed,";
-          if (x[i].getElementsByTagName("WindSpeed").HasChildNodes==true)
+          if (x[i].getElementsByTagName("WindSpeed").HasChildNodes())
             PM_data = PM_data + x[i].getElementsByTagName("WindSpeed")[0].childNodes[0].nodeValue; 
           PM_data = PM_data + ";"; 
         }
