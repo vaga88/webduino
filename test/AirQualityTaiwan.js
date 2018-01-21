ae992082c445e569f1a7ab5e22c81392b5d22cb2
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-21 18:26:17 +0800

    Update AirQualityTaiwan.js

--- test/AirQualityTaiwan.js
+++ test/AirQualityTaiwan.js
@@ -6,8 +6,8 @@
   
   function PM_gov_link(input_url_) 
   {
-    //CreateScript(input_url_);
-    AjaxCall(input_url_);
+    CreateScript(input_url_);
+    //AjaxCall(input_url_);
     
     if (window.XMLHttpRequest)
       var xmlHttp = new XMLHttpRequest();
