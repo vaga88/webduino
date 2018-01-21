8aa6d3862c80deb96239501ace1be2dbb0204e6e
fustyles <34397289+fustyles@users.noreply.github.com>
2018-01-21 20:46:20 +0800

    Update AirQualityTaiwan.js

--- test/AirQualityTaiwan.js
+++ test/AirQualityTaiwan.js
@@ -113,6 +113,8 @@
           });
           
           console.log(PM_data);
+          var json = $.parseJSON(json);
+          console.log(json.length);
           
           //"County,"",SiteName,"",PublishTime,"",Status,"",PM10,"",PM10_AVG,"",PM2.5,"",PM2.5_AVG,"",CO,"",CO_8hr,"",NO,"",NO2,"",NOx,"",O3,"",O3_8hr,"",SO2,"",WindDirec,"",WindSpeed,""             
         },
