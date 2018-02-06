031d6f73f4ba8a18315b58f0356591d47c1233da
fustyles <34397289+fustyles@users.noreply.github.com>
2018-02-06 19:32:27 +0800

    Update xmlHttp.js

--- xmlHTTP_20180206/xmlHttp.js
+++ xmlHTTP_20180206/xmlHttp.js
@@ -12,12 +12,6 @@
     DataUrl = input_url_;
     DataFormat = input_format_;
     xmlHTTP_get();
-    
-    var starttime=new Date().getTime();
-    while ((new Date().getTime()-starttime)<10000)
-    {
-      if(ResponseData!="") return ResponseData;
-    }
     return DataFormat;   
   } 
   
