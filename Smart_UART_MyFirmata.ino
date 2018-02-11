03675b075b45d277b88244d2a58f825526c7e0a8
fustyles <34397289+fustyles@users.noreply.github.com>
2018-02-11 15:30:16 +0800

    Update Smart_UART_MyFirmata.ino

--- Smart_UART_MyFirmata.ino
+++ Smart_UART_MyFirmata.ino
@@ -1,6 +1,6 @@
 // Webduino Smart UART
 
-// Author : ChungYi Fu (Kaohsiung, Taiwan)  2018-2-11 10:30 
+// Author : ChungYi Fu (Kaohsiung, Taiwan)  2018-2-11 15:30 
 
 // Command format :  
 // ?cmd  
@@ -21,75 +21,21 @@
 #include <SoftwareSerial.h>
 SoftwareSerial mySerial(10, 11); // Arduino RX:10, TX:11 
 
+String ReceiveData="", command="",cmd="",str1="",str2="";
+long int num1=-1,num2=-1;
+byte ReceiveState=0,cmdState=1,num1State=0,num2State=0,commastate=0;
+
 void setup()
 {
   Serial.begin(9600);
-  mySerial.begin(9600);  
+  mySerial.begin(9600);   
   mySerial.setTimeout(10);
   Serial.println("Hello World");
 }
 
 void loop() 
 {
-  String ReceiveData="", command="";
-  long int num1=-1,num2=-1;
-  String cmd="",str1="",str2="";
-  byte ReceiveState=0,cmdState=1,num1State=0,num2State=0,commastate=0;
-  
-  if (mySerial.available())
-  {
-    while (mySerial.available())
-    {
-      char c=mySerial.read();
-      delay(10);
-      ReceiveData=ReceiveData+String(c);
-      
-      if (String(c).indexOf("?")!=-1) ReceiveState=1;
-      if (String(c).indexOf(" ")!=-1) ReceiveState=0;
-      if ((ReceiveState==1)&&(String(c).indexOf("?")==-1)) 
-      {
-        command=command+String(c);
-
-        if ((String(c).indexOf("=")!=-1)&&(ReceiveState==1)) cmdState=0;
-        if (cmdState==1) cmd=cmd+String(c);
-
-        if ((String(c).indexOf("=")!=-1)&&(ReceiveState==1)) num1State=1;
-        if (((String(c).indexOf(",")!=-1)||(String(c).indexOf(" ")!=-1))&&(ReceiveState==1)) num1State=0;
-        if ((num1State==1)&&(String(c).indexOf("=")==-1))
-        {
-          if (ReceiveData.indexOf("?&")!=-1)
-            str1=str1+String(c);
-          else
-          {
-            if (num1==-1) 
-              num1=c-'0'; 
-            else
-              num1=num1*10+(c-'0'); 
-          }
-        }
-        
-        if ((String(c).indexOf(",")!=-1)&&(ReceiveState==1)) num2State=1;
-        if ((String(c).indexOf(" ")!=-1)&&(ReceiveState==1)) num2State=0;
-        if ((num2State==1)&&(String(c).indexOf(",")==-1))
-        {
-          if ((ReceiveData.indexOf("?&")!=-1)||(ReceiveData.indexOf("?+")!=-1))
-            str2=str2+String(c);
-          else
-          {          
-            if (num2==-1) 
-              num2=c-'0'; 
-            else
-              num2=num2*10+(c-'0'); 
-          }
-        }
-        else if ((num2State==1)&&(String(c).indexOf(",")!=-1)&&(commastate==1)&&((ReceiveData.indexOf("?&")!=-1)||(ReceiveData.indexOf("?+")!=-1)))
-          str2=str2+String(c); 
-        else if (num2State==1)
-          commastate=1;
-      }
-    }  
-    Serial.println(ReceiveData);
-  }
+  getVariable();
 
   if (ReceiveData.indexOf("?")==0)
   {
@@ -147,4 +93,66 @@ void loop()
 void SendData(String data)
 {
   mySerial.print(data);
+}
+
+void getVariable()
+{
+  ReceiveData="";command="";cmd="";str1="";str2="";
+  num1=-1,num2=-1;
+  byte ReceiveState=0,cmdState=1,num1State=0,num2State=0,commastate=0;
+  
+  if (mySerial.available())
+  {
+    while (mySerial.available())
+    {
+      char c=mySerial.read();
+      delay(10);
+      ReceiveData=ReceiveData+String(c);
+      
+      if (String(c).indexOf("?")!=-1) ReceiveState=1;
+      if (String(c).indexOf(" ")!=-1) ReceiveState=0;
+      if ((ReceiveState==1)&&(String(c).indexOf("?")==-1)) 
+      {
+        command=command+String(c);
+
+        if ((String(c).indexOf("=")!=-1)&&(ReceiveState==1)) cmdState=0;
+        if (cmdState==1) cmd=cmd+String(c);
+
+        if ((String(c).indexOf("=")!=-1)&&(ReceiveState==1)) num1State=1;
+        if (((String(c).indexOf(",")!=-1)||(String(c).indexOf(" ")!=-1))&&(ReceiveState==1)) num1State=0;
+        if ((num1State==1)&&(String(c).indexOf("=")==-1))
+        {
+          if (ReceiveData.indexOf("?&")!=-1)
+            str1=str1+String(c);
+          else
+          {
+            if (num1==-1) 
+              num1=c-'0'; 
+            else
+              num1=num1*10+(c-'0'); 
+          }
+        }
+        
+        if ((String(c).indexOf(",")!=-1)&&(ReceiveState==1)) num2State=1;
+        if ((String(c).indexOf(" ")!=-1)&&(ReceiveState==1)) num2State=0;
+        if ((num2State==1)&&(String(c).indexOf(",")==-1))
+        {
+          if ((ReceiveData.indexOf("?&")!=-1)||(ReceiveData.indexOf("?+")!=-1))
+            str2=str2+String(c);
+          else
+          {          
+            if (num2==-1) 
+              num2=c-'0'; 
+            else
+              num2=num2*10+(c-'0'); 
+          }
+        }
+        else if ((num2State==1)&&(String(c).indexOf(",")!=-1)&&(commastate==1)&&((ReceiveData.indexOf("?&")!=-1)||(ReceiveData.indexOf("?+")!=-1)))
+          str2=str2+String(c); 
+        else if (num2State==1)
+          commastate=1;
+      }
+    }  
+    Serial.println(ReceiveData);
+  }
 }
