d7d5b5a1ab188f9414a3c4666fdd6d189889a0b4
fustyles <34397289+fustyles@users.noreply.github.com>
2018-02-19 20:13:24 +0800

    Update zh-hans.js

--- RGB_LED_20180219/blockly/msg/blocks/zh-hans.js
+++ RGB_LED_20180219/blockly/msg/blocks/zh-hans.js
@@ -1,2 +1,7 @@
-Blockly.Msg.PM_LINK_SHOW = "连结空气资料";
-Blockly.Msg.PM_GET_SHOW = "取得空气资料";
+Blockly.Msg.TITLE_SHOW = "RGB LED   ";
+Blockly.Msg.RED_SHOW = "RED";
+Blockly.Msg.VALE_RED_SHOW = "VALUE";
+Blockly.Msg.GREEN_SHOW = "GREEN";
+Blockly.Msg.VALUE_GREEN_SHOW = "VALUE";
+Blockly.Msg.BLUE_SHOW = "BLUE";
+Blockly.Msg.VALUE_BLUE_SHOW = "VALUE";
