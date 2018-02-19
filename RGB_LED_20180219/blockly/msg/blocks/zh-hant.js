a4305dd971b6a0313e07bde81ab5fb3ef699edd9
fustyles <34397289+fustyles@users.noreply.github.com>
2018-02-19 20:18:03 +0800

    Update zh-hant.js

--- RGB_LED_20180219/blockly/msg/blocks/zh-hant.js
+++ RGB_LED_20180219/blockly/msg/blocks/zh-hant.js
@@ -1,7 +1,7 @@
-Blockly.Msg.TITLE_SHOW = "RGB LED   ";
-Blockly.Msg.RED_SHOW = "RED";
-Blockly.Msg.VALE_RED_SHOW = "VALUE";
-Blockly.Msg.GREEN_SHOW = "GREEN";
-Blockly.Msg.VALUE_GREEN_SHOW = "VALUE";
-Blockly.Msg.BLUE_SHOW = "BLUE";
-Blockly.Msg.VALUE_BLUE_SHOW = "VALUE";
+Blockly.Msg.TITLE_SHOW = "三色LED   ";
+Blockly.Msg.RED_SHOW = "紅";
+Blockly.Msg.VALE_RED_SHOW = "數值";
+Blockly.Msg.GREEN_SHOW = "綠";
+Blockly.Msg.VALUE_GREEN_SHOW = "數值";
+Blockly.Msg.BLUE_SHOW = "藍";
+Blockly.Msg.VALUE_BLUE_SHOW = "數值";
