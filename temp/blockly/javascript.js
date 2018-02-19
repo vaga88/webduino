ad9131d00cad6b954ede1449238e1bf9b996689d
fustyles <34397289+fustyles@users.noreply.github.com>
2018-02-19 19:11:12 +0800

    Update javascript.js

--- temp/blockly/javascript.js
+++ temp/blockly/javascript.js
@@ -1,12 +1,20 @@
-Blockly.JavaScript['pmgov_link'] = function(block) {
-  var value_format_ = block.getFieldValue('value_format_'); 
-  var value_url_ = Blockly.JavaScript.valueToCode(block, 'value_url_', Blockly.JavaScript.ORDER_ATOMIC);  
-  var code = 'PM_gov_link('+value_url_+',"'+value_format_+'");\nawait delay(1);';
+Blockly.JavaScript['rgb_led'] = function(block) {
+  var dropdown_pin_red = block.getFieldValue('PIN_RED');
+  var value_value_red = Blockly.JavaScript.valueToCode(block, 'VALUE_RED', Blockly.JavaScript.ORDER_ATOMIC);
+  var dropdown_pin_green = block.getFieldValue('PIN_GREEN');
+  var value_value_green = Blockly.JavaScript.valueToCode(block, 'VALUE_GREEN', Blockly.JavaScript.ORDER_ATOMIC);
+  var dropdown_pin_blue = block.getFieldValue('PIN_BLUE');
+  var value_value_blue = Blockly.JavaScript.valueToCode(block, 'VALUE_BLUE', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'var pinred = getPin(board, '+pin_red+');\n'+
+             'pinred.setMode(3);\n'+
+             'pinred.write('+value_red+');\n'+
+             'var pingreen = getPin(board, '+pin_green+');\n'+
+             'pingreen.setMode(3);\n'+
+             'pingreen.write('+value_green+');\n'+
+             'var pinblue = getPin(board, '+pin_blue+');\n'+
+             'pinblue.setMode(3);\n'+
+             'pinblue.write('+value_blue+');\n'+
+             'funRGB_LED("Success");\n';
   return code;
-};
-
-Blockly.JavaScript['pmgov_get'] = function(block) {
-  var value_site_ = block.getFieldValue('value_site_');  
-  var code = 'PM_gov_get("'+value_site_+'")';
-  return [code, Blockly.JavaScript.ORDER_NONE];
 };
