f8cc6f434cd4a9e5710f8e4d057e08ca6f8a5b7f
fustyles <34397289+fustyles@users.noreply.github.com>
2018-02-19 21:27:09 +0800

    Update javascript.js

--- RGB_LED_20180219/blockly/javascript.js
+++ RGB_LED_20180219/blockly/javascript.js
@@ -6,16 +6,18 @@ Blockly.JavaScript['rgb_led'] = function(block) {
   var pin_blue = block.getFieldValue('PIN_BLUE');
   var value_blue = Blockly.JavaScript.valueToCode(block, 'VALUE_BLUE', Blockly.JavaScript.ORDER_ATOMIC);
   
+  Blockly.JavaScript.definitions_['var_rgbledpin'] = 'var pinred,pingreen,pinblue;';
+  
   // TODO: Assemble JavaScript into code variable.
-  var code = 'var pinred = getPin(board, '+pin_red+');\n'+
+  var code = 'pinred = getPin(board, '+pin_red+');\n'+
              'pinred.setMode(3);\n'+
              'pinred.write('+value_red+');\n'+
-             'var pingreen = getPin(board, '+pin_green+');\n'+
+             'pingreen = getPin(board, '+pin_green+');\n'+
              'pingreen.setMode(3);\n'+
              'pingreen.write('+value_green+');\n'+
-             'var pinblue = getPin(board, '+pin_blue+');\n'+
+             'pinblue = getPin(board, '+pin_blue+');\n'+
              'pinblue.setMode(3);\n'+
-             'pinblue.write('+value_blue+');\n'+
+             'pinblue.write('+value_blue+');\n\n'+
              'funRGB_LED("Success");\n';
   return code;
 };
