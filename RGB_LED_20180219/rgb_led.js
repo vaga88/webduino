a22cb89a859f043d1cac20381e47783b257b5381
fustyles <34397289+fustyles@users.noreply.github.com>
2018-02-19 18:30:44 +0800

    Update rgb_led.js

--- RGB_LED_20180219/rgb_led.js
+++ RGB_LED_20180219/rgb_led.js
@@ -3,17 +3,9 @@
   'use strict';
   
   
-  function funRGB_LED(board,pin_red,value_red,pin_green,value_green,pin_blue,value_blue)
+  function funRGB_LED(pin_red,value_red,pin_green,value_green,pin_blue,value_blue)
   {
-    var pin = getPin(board, pin_red);
-    pin.setMode(3);
-    pin.write(value_red);
-    var pin = getPin(board, pin_green);
-    pin.setMode(3);
-    pin.write(value_green);
-    var pin = getPin(board, pin_blue);
-    pin.setMode(3);
-    pin.write(value_blue);    
+
   }
     
   window.funRGB_LED = funRGB_LED;
