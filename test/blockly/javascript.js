c8cc4293dc9d8cfab411269c9ec8ff18f0650030
fustyles <34397289+fustyles@users.noreply.github.com>
2018-03-08 10:31:14 +0800

    Update javascript.js

--- test/blockly/javascript.js
+++ test/blockly/javascript.js
@@ -29,3 +29,18 @@ Blockly.JavaScript['uart_data'] = function(block) {
     var code = variable_uart + '.' + dropdown_dataType;
     return [code, Blockly.JavaScript.ORDER_NONE];
 };
+
+Blockly.JavaScript['uart_car'] = function(block) {
+  var value_cmd = Blockly.JavaScript.valueToCode(block, 'cmd', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_str1 = Blockly.JavaScript.valueToCode(block, 'str1', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_str2 = Blockly.JavaScript.valueToCode(block, 'str2', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_str3 = Blockly.JavaScript.valueToCode(block, 'str3', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_str4 = Blockly.JavaScript.valueToCode(block, 'str4', Blockly.JavaScript.ORDER_ATOMIC);
+  var dropdown_str5 = block.getFieldValue('str5');
+  var value_str6 = Blockly.JavaScript.valueToCode(block, 'str6', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_str7 = Blockly.JavaScript.valueToCode(block, 'str7', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
