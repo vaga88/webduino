92780d463d74738ef70f09195c8b31130ef1dea6
fustyles <34397289+fustyles@users.noreply.github.com>
2018-03-08 16:07:26 +0800

    Update uartmyfirmata.js

--- test/uartmyfirmata.js
+++ test/uartmyfirmata.js
@@ -4,17 +4,17 @@
   
   function uartcar(cmd,str1,str2,str3,str4,str5,str6,str7)
   {
-    // "+value_cmd+","+value_str1+","+value_str2+","+value_str3+","+value_str4+","+dropdown_str5+","+value_str6+","+value_str7
+    return "?"+cmd+"="+str1+";"+str2+";"+str3+";"+str4+";"+str5+";"+str6+";"+str7;
   }
   
   function uartsystem(cmd,str1,str2,str3)
   {
-    
+    return "?"+cmd+"="+str1+";"+str2+";"+str3;
   }
   
   function uartcustom(cmd,str1,str2,str3,str4,str5,str6,str7,str8,str9)
   {
-    
+    return "?"+cmd+"="+str1+";"+str2+";"+str3+";"+str4+";"+str5+";"+str6+";"+str7+";"+str8+";"+str9;
   }
   
   window.uartcar = uartcar;
