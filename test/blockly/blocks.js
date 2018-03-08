5f004a57833d132862d7eaa3616843d50cfaafab
fustyles <34397289+fustyles@users.noreply.github.com>
2018-03-08 10:30:10 +0800

    Update blocks.js

--- test/blockly/blocks.js
+++ test/blockly/blocks.js
@@ -62,3 +62,43 @@ Blockly.Blocks['uart_data'] = {
         this.setHelpUrl("");
     }
 };
+
+Blockly.Blocks['uart_car'] = {
+  init: function() {
+    this.appendValueInput("cmd")
+        .setCheck(null)
+        .appendField("Uart Car     cmd");
+    this.appendValueInput("str1")
+        .setCheck(null)
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("pin(A-1A)");
+    this.appendValueInput("str2")
+        .setCheck(null)
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("pin(A-1B)");
+    this.appendValueInput("str3")
+        .setCheck(null)
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("pin(B-1A)");
+    this.appendValueInput("str4")
+        .setCheck(null)
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("pin((B-1B)");
+    this.appendDummyInput()
+        .appendField("STATE")
+        .appendField(new Blockly.FieldDropdown([["forward","F"], ["backward","B"], ["left","L"], ["right","R"], ["stop","S"]]), "str5");
+    this.appendValueInput("str6")
+        .setCheck(null)
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("A speed");
+    this.appendValueInput("str7")
+        .setCheck(null)
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("B speed");
+    this.setInputsInline(true);
+    this.setOutput(true, null);
+    this.setColour(315);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
