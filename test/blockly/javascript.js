9bcad6b93eb0236349efb4dd0ea79a483b143d6e
fustyles <34397289+fustyles@users.noreply.github.com>
2018-03-08 13:08:43 +0800

    Update javascript.js

--- test/blockly/javascript.js
+++ test/blockly/javascript.js
@@ -7,7 +7,8 @@ Blockly.JavaScript['uart_car'] = function(block) {
   var dropdown_str5 = block.getFieldValue('str5');
   var value_str6 = Blockly.JavaScript.valueToCode(block, 'str6', Blockly.JavaScript.ORDER_ATOMIC);
   var value_str7 = Blockly.JavaScript.valueToCode(block, 'str7', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = '?'+value_cmd+'='+value_str1+';'+value_str2+';'+value_str3+';'+value_str4+';'+dropdown_str5+';'+value_str6+';'+value_str7;
+  var cmd = '?'+value_cmd+'='+value_str1+';'+value_str2+';'+value_str3+';'+value_str4+';'+dropdown_str5+';'+value_str6+';'+value_str7;
+  var code = cmd;
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
@@ -16,7 +17,8 @@ Blockly.JavaScript['uart_system'] = function(block) {
   var value_str1 = Blockly.JavaScript.valueToCode(block, 'str1', Blockly.JavaScript.ORDER_ATOMIC);
   var value_str2 = Blockly.JavaScript.valueToCode(block, 'str2', Blockly.JavaScript.ORDER_ATOMIC);
   var value_str3 = Blockly.JavaScript.valueToCode(block, 'str3', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = '?'+dropdown_cmd+'='+value_str1+';'+value_str2+';'+value_str3;
+  var cmd = '?'+dropdown_cmd+'='+value_str1+';'+value_str2+';'+value_str3;
+  var code = cmd;
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
@@ -31,6 +33,7 @@ Blockly.JavaScript['uart_custom'] = function(block) {
   var value_str7 = Blockly.JavaScript.valueToCode(block, 'str7', Blockly.JavaScript.ORDER_ATOMIC);
   var value_str8 = Blockly.JavaScript.valueToCode(block, 'str8', Blockly.JavaScript.ORDER_ATOMIC);
   var value_str9 = Blockly.JavaScript.valueToCode(block, 'str9', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = '?'+value_cmd+'='+value_str1+';'+value_str2+';'+value_str3+';'+value_str4+';'+value_str5+';'+value_str6+';'+value_str7+';'+value_str8+';'+value_str9;
+  var cmd = '?'+value_cmd+'='+value_str1+';'+value_str2+';'+value_str3+';'+value_str4+';'+value_str5+';'+value_str6+';'+value_str7+';'+value_str8+';'+value_str9;
+  var code = cmd;
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
