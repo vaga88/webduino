51ff3383a1ddab99ad976a026a3abe1075d5af8b
fustyles <34397289+fustyles@users.noreply.github.com>
2018-03-09 17:36:33 +0800

    Update uartmyfirmata.js

--- test/uartmyfirmata.js
+++ test/uartmyfirmata.js
@@ -2,9 +2,9 @@
 
   'use strict';
   
-  function uartcar(cmd,str1,str2,str3,str4,str5,str6,str7)
+  function uartcar(cmd,str1,str2,str3,str4,str5,str6,str7,str8)
   {
-    return "?"+cmd+"="+str1+";"+str2+";"+str3+";"+str4+";"+str5+";"+str6+";"+str7;
+    return "?"+cmd+"="+str1+";"+str2+";"+str3+";"+str4+";"+str5+";"+str6+";"+str7+";"+str8;
   }
   
   function uartsystem(cmd,str1,str2,str3)
