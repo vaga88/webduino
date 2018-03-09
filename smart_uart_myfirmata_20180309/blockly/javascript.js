43c7516516ddeeeaae2b625139f30e63215f0e92
fustyles <34397289+fustyles@users.noreply.github.com>
2018-03-09 22:11:17 +0800

    Update javascript.js

--- smart_uart_myfirmata_20180309/blockly/javascript.js
+++ smart_uart_myfirmata_20180309/blockly/javascript.js
@@ -4,11 +4,11 @@ Blockly.JavaScript['uart_car'] = function(block) {
   var value_str2 = Blockly.JavaScript.valueToCode(block, 'str2', Blockly.JavaScript.ORDER_ATOMIC);
   var value_str3 = Blockly.JavaScript.valueToCode(block, 'str3', Blockly.JavaScript.ORDER_ATOMIC);
   var value_str4 = Blockly.JavaScript.valueToCode(block, 'str4', Blockly.JavaScript.ORDER_ATOMIC);
-  var dropdown_str5 = block.getFieldValue('str5');
+  var value_str5 = Blockly.JavaScript.valueToCode(block, 'str5', Blockly.JavaScript.ORDER_ATOMIC);
   var value_str6 = Blockly.JavaScript.valueToCode(block, 'str6', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_str7 = Blockly.JavaScript.valueToCode(block, 'str7', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_str8 = Blockly.JavaScript.valueToCode(block, 'str8', Blockly.JavaScript.ORDER_ATOMIC);  
-  var code = "uartcar("+value_cmd+","+value_str1+","+value_str2+","+value_str3+","+value_str4+",'"+dropdown_str5+"',"+value_str6+","+value_str7+","+value_str8+")";
+  var value_str7 = Blockly.JavaScript.valueToCode(block, 'str7', Blockly.JavaScript.ORDER_ATOMIC);  
+  var dropdown_str8 = block.getFieldValue('str8');
+  var code = "uartcar("+value_cmd+","+value_str1+","+value_str2+","+value_str3+","+value_str4+","+value_str5+","+value_str6+","+value_str7+",'"+dropdown_str8+"')";
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
