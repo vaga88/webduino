ad32c9ce6d5b7b685f7d8b7acf637d22030cb41b
fustyles <34397289+fustyles@users.noreply.github.com>
2018-03-13 07:43:28 +0800

    Update ifttt.js

--- IFTTT_20180312/ifttt.js
+++ IFTTT_20180312/ifttt.js
@@ -5,12 +5,12 @@
   function createIframe(ifttt_eventname,ifttt_key,ifttt_value1,ifttt_value2,ifttt_value3) {
     
     var input_url="https://maker.ifttt.com/trigger/"+ifttt_eventname+"/with/key/"+ifttt_key+"?value1="+ifttt_value1+"&value2="+ifttt_value2+"&value3="+ifttt_value3;
-    var src = '<iframe src="' + input_url + '" style="width:400px;height:50px"></iframe>';
+    var src = '<iframe src="' + input_url + '" style="width:0px;height:0px"></iframe>';
     
     if (document.getElementById("fustyles_iframe"))
     {
           document.getElementById("fustyles_iframe").style.left = '0px';
-          document.getElementById("fustyles_iframe").style.top = '100px';
+          document.getElementById("fustyles_iframe").style.top = '0px';
           document.getElementById("fustyles_iframe").style.display = 'block';
           document.getElementById("fustyles_iframe").innerHTML = src;
     }
@@ -20,7 +20,7 @@
         div.id = "fustyles_iframe";
         div.style.position = 'absolute';      
         div.style.left = '0px';
-        div.style.top = '100px';
+        div.style.top = '0px';
         div.style.zindex='9999';      
         div.innerHTML = src;
         document.body.appendChild(div);
