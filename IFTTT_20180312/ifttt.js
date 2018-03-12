17af96fa8dda1b40cfda4c263ee77b68670be89b
fustyles <34397289+fustyles@users.noreply.github.com>
2018-03-13 00:15:38 +0800

    Update ifttt.js

--- IFTTT_20180312/ifttt.js
+++ IFTTT_20180312/ifttt.js
@@ -2,33 +2,25 @@
 
   'use strict';
 
-  function createIframe(input_id_,input_url_,input_WIDTH_,input_HEIGHT_,input_LEFT_,input_TOP_) {
+  function createIframe(ifttt_eventname,ifttt_key,ifttt_value1,ifttt_value2,ifttt_value3) {
     
-    var substr = input_url_.toLowerCase().split('.');
-    var extname_av = ".aac,.au,.aif,.aiff,.aiffc,.alac,.ape,.asf,.avi,.cda,.dat,.divx,.flac,.m2ts,.m4a,.mid,.mov,.mp2,.mp3,.mp4,.mpc,.mpg,.mpeg,.mv2,.ogg,.pdf,.ra,.ram,.raw,.rcp,.rm,.rmvb,.snd,.tak,.tta,.vob,.voc,.wma,.wav,.webm,.wmf,.wmv,.wv,.xmi,";
-    var extname_flash = ".swf,.flv,";
-        
-    if (extname_av.indexOf("."+substr[substr.length-1]+",")!=-1)
-        var src = '<embed src="' + input_url_ + '" style="width:' + input_WIDTH_ + 'px;height:' + input_HEIGHT_ + 'px" autostart="true"></embed>'; 
-    else if (extname_flash.indexOf("."+substr[substr.length-1]+",")!=-1)
-        var src = '<embed src="' + input_url_ + '" style="width:' + input_WIDTH_ + 'px;height:' + input_HEIGHT_ + 'px" type="application/x-shockwave-flash"></embed>';
-    else
-        var src = '<iframe src="' + input_url_ + '" style="width:' + input_WIDTH_ + 'px;height:' + input_HEIGHT_ + 'px"></iframe>';
+    var input_url="https://maker.ifttt.com/trigger/"+ifttt_eventname+"/with/key/"+ifttt_key+"?value1="+ifttt_value1+"&value2="+ifttt_value2+"&value3="+ifttt_value3;
+    var src = '<iframe src="' + input_url + '" style="width:0px;height:0px"></iframe>';
     
-    if (document.getElementById("fustyles_iframe"+input_id_))
+    if (document.getElementById("fustyles_iframe"))
     {
-          document.getElementById("fustyles_iframe"+input_id_).style.left = input_LEFT_ + 'px';
-          document.getElementById("fustyles_iframe"+input_id_).style.top = input_TOP_ + 'px';
-          document.getElementById("fustyles_iframe"+input_id_).style.display = 'block';
-          document.getElementById("fustyles_iframe"+input_id_).innerHTML = src;
+          document.getElementById("fustyles_iframe").style.left = '0px';
+          document.getElementById("fustyles_iframe").style.top = '0px';
+          document.getElementById("fustyles_iframe").style.display = 'block';
+          document.getElementById("fustyles_iframe").innerHTML = src;
     }
     else
     {
         var div = document.createElement('div');
-        div.id = "fustyles_iframe"+input_id_;
+        div.id = "fustyles_iframe";
         div.style.position = 'absolute';      
-        div.style.left = input_LEFT_ + 'px';
-        div.style.top = input_TOP_ + 'px';
+        div.style.left = '0px';
+        div.style.top = '0px';
         div.style.zindex='9999';      
         div.innerHTML = src;
         document.body.appendChild(div);
