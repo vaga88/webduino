86264f7b9014c44211dbfe878685f3b8e61f52ed
fustyles <34397289+fustyles@users.noreply.github.com>
2018-03-13 00:01:10 +0800

    Update IFTTT.js

--- IFTTT_20180312/IFTTT.js
+++ IFTTT_20180312/IFTTT.js
@@ -2,25 +2,25 @@
 
   'use strict';
 
-  function createIframe(ifttt_eventname,ifttt_key,ifttt_value1,ifttt_value2,ifttt_value3,input_id_,input_WIDTH_,input_HEIGHT_,input_LEFT_,input_TOP_) {
+  function createIframe(ifttt_eventname,ifttt_key,ifttt_value1,ifttt_value2,ifttt_value3) {
     
     var input_url="https://maker.ifttt.com/trigger/"+ifttt_eventname+"/with/key/"+ifttt_key+"?value1="+ifttt_value1+"&value2="+ifttt_value2+"&value3="+ifttt_value3;
-    var src = '<iframe src="' + input_url + '" style="width:' + input_WIDTH_ + 'px;height:' + input_HEIGHT_ + 'px"></iframe>';
+    var src = '<iframe src="' + input_url + '" style="width:0px;height:0px"></iframe>';
     
-    if (document.getElementById("fustyles_iframe"+input_id_))
+    if (document.getElementById("fustyles_iframe"))
     {
-          document.getElementById("fustyles_iframe"+input_id_).style.left = input_LEFT_ + 'px';
-          document.getElementById("fustyles_iframe"+input_id_).style.top = input_TOP_ + 'px';
-          document.getElementById("fustyles_iframe"+input_id_).style.display = 'block';
-          document.getElementById("fustyles_iframe"+input_id_).innerHTML = src;
+          document.getElementById("fustyles_iframe").style.left = '0px';
+          document.getElementById("fustyles_iframe").style.top = '0px';
+          document.getElementById("fustyles_iframe").style.display = 'block';
+          document.getElementById("fustyles_iframe").innerHTML = src;
     }
     else
     {
         var div = document.createElement('div');
-        div.id = "fustyles_iframe"+input_id_;
+        div.id = "fustyles_iframe";
         div.style.position = 'absolute';      
-        div.style.left = input_LEFT_ + 'px';
-        div.style.top = input_TOP_ + 'px';
+        div.style.left = '0px';
+        div.style.top = '0px';
         div.style.zindex='9999';      
         div.innerHTML = src;
         document.body.appendChild(div);
