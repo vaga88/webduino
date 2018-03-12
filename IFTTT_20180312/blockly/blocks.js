45ce35132f61b34237fa4356c9b5c967fa7177b4
fustyles <34397289+fustyles@users.noreply.github.com>
2018-03-13 00:09:49 +0800

    Update blocks.js

--- IFTTT_20180312/blockly/blocks.js
+++ IFTTT_20180312/blockly/blocks.js
@@ -1,48 +1,30 @@
 Blockly.Blocks['iframe_open'] = {
   init: function() {
-  this.appendValueInput("openid_")
-      .setCheck("Number")
-      .setAlign(Blockly.ALIGN_RIGHT)
-      .appendField(Blockly.Msg.OPEN_SHOW)
-      .appendField(Blockly.Msg.OPENID_SHOW)    
-  this.appendValueInput("url_")
+  this.appendValueInput("ifttt_eventname")
       .setCheck("String")
       .setAlign(Blockly.ALIGN_RIGHT)
-      .appendField(Blockly.Msg.URL_SHOW)
-  this.appendValueInput("width_")
-      .setCheck("Number")
+      .appendField("IFTTT   ")
+      .appendField("EventName")    
+  this.appendValueInput("ifttt_key")
+      .setCheck("String")
       .setAlign(Blockly.ALIGN_RIGHT)
-      .appendField(Blockly.Msg.WIDTH_SHOW)
-  this.appendValueInput("height_")
-      .setCheck("Number")
+      .appendField("Key")
+  this.appendValueInput("ifttt_value1")
+      .setCheck("String")
       .setAlign(Blockly.ALIGN_RIGHT)
-      .appendField(Blockly.Msg.HEIGHT_SHOW);
-  this.appendValueInput("left_")
-      .setCheck("Number")
+      .appendField("Value1")
+  this.appendValueInput("ifttt_value2")
+      .setCheck("String")
       .setAlign(Blockly.ALIGN_RIGHT)
-      .appendField(Blockly.Msg.LEFT_SHOW);    
-    this.appendValueInput("top_")
-      .setCheck("Number")
+      .appendField("Value2")
+  this.appendValueInput("ifttt_value3")
+      .setCheck("String")
       .setAlign(Blockly.ALIGN_RIGHT)
-      .appendField(Blockly.Msg.TOP_SHOW);  
+      .appendField("Value3")  
   this.setPreviousStatement(true);
   this.setNextStatement(true);
   this.setTooltip('');
   this.setColour(65);
   this.setHelpUrl(mainUrl + 'basic/blockly/blockly-information.html' + utmUrl);
-  }
-};
-Blockly.Blocks['iframe_close'] = {
-  init: function() {
-    this.appendValueInput("closeid_")
-        .setCheck("Number")
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField(Blockly.Msg.CLOSE_SHOW)
-        .appendField(Blockly.Msg.CLOSEID_SHOW)     
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(65);
- this.setTooltip("");
- this.setHelpUrl("");
   }
 };
