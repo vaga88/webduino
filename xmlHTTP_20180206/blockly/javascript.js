0d9cccad8625223712395df2e8b54dcf87b9e2be
fustyles <34397289+fustyles@users.noreply.github.com>
2018-04-01 22:16:29 +0800

    Update javascript.js

--- xmlHTTP_20180206/blockly/javascript.js
+++ xmlHTTP_20180206/blockly/javascript.js
@@ -1,6 +1,6 @@
 Blockly.JavaScript['xmlHTTP_ResponseData'] = function(block) {
   var value_format_ = block.getFieldValue('value_format_'); 
   var value_url_ = Blockly.JavaScript.valueToCode(block, 'value_url_', Blockly.JavaScript.ORDER_ATOMIC);  
-  var code = 'xmlHTTP('+value_url_+',"'+value_format_+'")';
+  var code = 'getresponse('+value_url_+',"'+value_format_+'")';
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
